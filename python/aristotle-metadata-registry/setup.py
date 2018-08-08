0ead1945e2a220126373e9d37176b4e0122b07e2
Harry White <harry.white424@gmail.com>
2018-08-09 09:26:51 +1000

    no alpha versions

--- python/aristotle-metadata-registry/setup.py
+++ python/aristotle-metadata-registry/setup.py
@@ -85,7 +85,7 @@ setup(
         'django-organizations',
 
         # Improved User Model
-        'django-improved-user>=1.0.0',
+        'django-improved-user>=1.0.0,!=1.0a1,!=1.0a2',
 
         # File upload
         'django-constrainedfilefield[filetype]',
