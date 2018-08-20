12e96b1e949742d3da416d552fc40e34f4bd7bcc
Harry White <harry.white424@gmail.com>
2018-08-20 16:48:04 +1000

    2 lines class

--- python/aristotle-metadata-registry/aristotle_mdr/contrib/user_management/models.py
+++ python/aristotle-metadata-registry/aristotle_mdr/contrib/user_management/models.py
@@ -3,5 +3,6 @@ from aristotle_mdr.fields import LowerEmailField
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+
 class User(AbstractUser):
     email = LowerEmailField(_('email address'), max_length=254, unique=True)
