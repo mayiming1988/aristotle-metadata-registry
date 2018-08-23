1adfa9384f156eb1b38be85a5ad8dabb7654bbf2
Harry White <harry.white424@gmail.com>
2018-08-23 13:35:21 +1000

    Change diff to specific commit

--- scripts/check_path.sh
+++ scripts/check_path.sh
@@ -1,4 +1,4 @@
-if git diff --name-only $TRAVIS_BRANCH...HEAD|grep ^$1 &>/dev/null; then
+if git diff --name-only $TRAVIS_BRANCH $TRAVIS_COMMIT|grep ^$1 &>/dev/null; then
     # Folder is in latest check in
     exit 0
 else
