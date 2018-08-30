c35b38a7073360c0906ca131ece3fe915ea2a604
Harry White <harry.white424@gmail.com>
2018-08-30 10:52:02 +1000

    Added lazy profile context processor

--- python/aristotle-metadata-registry/aristotle_mdr/context_processors.py
+++ python/aristotle-metadata-registry/aristotle_mdr/context_processors.py
@@ -1,7 +1,9 @@
 from aristotle_mdr.utils import fetch_aristotle_settings
 from aristotle_mdr.views.bulk_actions import get_bulk_actions
+from aristotle_mdr.models import PossumProfile
 from django.contrib.auth.context_processors import PermWrapper
 from django.contrib.auth import get_user_model
+from django.utils.functional import SimpleLazyObject
 
 
 # This allows us to pass the Aristotle settings through to the final rendered page
@@ -11,18 +13,17 @@ def settings(request):
         'bulk_actions': get_bulk_actions(),
     }
 
-def auth(request):
+def get_profile(request):
+    return PossumProfile.objects.get(user=request.user)
+
+def profile(request):
     """
-    Replacement for the django auth context processor, with some prefetching for aristotle
-    Adapted from the original at django.contrib.auth.context_processors
+    Context processor providing lazy profile object
+    Not currently used
     """
-    if hasattr(request, 'user'):
-        user = get_user_model().objects.filter(pk=request.user.pk).select_related('profile').first()
-    else:
-        from django.contrib.auth.models import AnonymousUser
-        user = AnonymousUser()
+
+    lazy_profile = SimpleLazyObject(lambda: get_profile(request))
 
     return {
-        'user': user,
-        'perms': PermWrapper(user),
+        'profile': lazy_profile
     }
--- python/aristotle-metadata-registry/aristotle_mdr/required_settings.py
+++ python/aristotle-metadata-registry/aristotle_mdr/required_settings.py
@@ -40,8 +40,8 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                #'django.contrib.auth.context_processors.auth',
-                'aristotle_mdr.context_processors.auth',
+                'django.contrib.auth.context_processors.auth',
+                #'aristotle_mdr.context_processors.profile',
                 'django.template.context_processors.request',
                 'django.template.context_processors.static',
                 'aristotle_mdr.context_processors.settings',
