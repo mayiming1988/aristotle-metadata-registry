86b98c139b8e8e3060f2e33e8cc2315e80860fce
Harry White <harry.white424@gmail.com>
2018-09-05 11:06:53 +1000

    Console print, tags array

--- python/aristotle-metadata-registry/aristotle_mdr/static/aristotle_mdr/itemPage.js
+++ python/aristotle-metadata-registry/aristotle_mdr/static/aristotle_mdr/itemPage.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
 
   var tag_editor = new Taggle('taggle-editor');
+
+  $('#tag-editor-submit').click(function() {
+    console.log(tag_editor.getTags().values)
+  })
+
 })
