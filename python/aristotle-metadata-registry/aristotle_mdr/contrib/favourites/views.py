5e0bcde1750bdc29f4d5e77cba883b1362468439
Harry White <harry.white424@gmail.com>
2018-09-05 12:12:34 +1000

    Async favourites

--- python/aristotle-metadata-registry/aristotle_mdr/contrib/favourites/views.py
+++ python/aristotle-metadata-registry/aristotle_mdr/contrib/favourites/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import get_object_or_404
 from aristotle_mdr.utils import url_slugify_concept
 from django.contrib.auth.decorators import login_required
 from aristotle_mdr.models import _concept
@@ -7,7 +7,7 @@ from django.utils.translation import ugettext_lazy as _
 from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
-from django.http.response import JsonResponse
+from django.http.response import JsonResponse, HttpResponseRedirect
 
 
 class ToggleFavourite(LoginRequiredMixin, View):
@@ -22,7 +22,7 @@ class ToggleFavourite(LoginRequiredMixin, View):
         favourited = request.user.profile.toggleFavourite(item)
 
         if request.is_ajax():
-            return self.get_json_response(favourited)
+            return self.get_json_response(item, favourited)
         else:
             return self.redirect_with_message(item, favourited)
 
@@ -41,7 +41,7 @@ class ToggleFavourite(LoginRequiredMixin, View):
     def redirect_with_message(self, item, favourited):
         message = self.get_message(item, favourited)
         messages.add_message(self.request, messages.SUCCESS, message)
-        return redirect(url_slugify_concept(item))
+        return HttpResponseRedirect(url_slugify_concept(item))
 
     def get_json_response(self, item, favourited):
         message = self.get_message(item, favourited)
--- python/aristotle-metadata-registry/aristotle_mdr/static/aristotle_mdr/itemPage.js
+++ python/aristotle-metadata-registry/aristotle_mdr/static/aristotle_mdr/itemPage.js
@@ -6,4 +6,24 @@ $(document).ready(function() {
     console.log(tag_editor.getTags().values)
   })
 
+  // Async favouriting
+  var fav_button = $('.btn.favourite').first()
+  var fav_url = fav_button.attr('href')
+  fav_button.removeAttr('href')
+
+  fav_button.click(function() {
+    $.get(
+      fav_url,
+      function(data) {
+        console.log(data)
+        var i = fav_button.find('i').first()
+        if (data.favourited) {
+          i.removeClass('fa-bookmark-o').addClass('fa-bookmark')
+        } else {
+          i.removeClass('fa-bookmark').addClass('fa-bookmark-o')
+        }
+      }
+    )
+  })
+
 })
