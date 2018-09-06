7c058939d646fc06f0acc6bfb56538721e0e6b6e
Harry White <harry.white424@gmail.com>
2018-09-06 16:45:59 +1000

    Switch to vue simple suggest

--- python/aristotle-metadata-registry/aristotle_mdr/static/aristotle_mdr/itemPage.js
+++ python/aristotle-metadata-registry/aristotle_mdr/static/aristotle_mdr/itemPage.js
@@ -25,35 +25,42 @@ $(document).ready(function() {
 
 tagComponent = {
   template: '<div><div id="taggle-editor" class="input taggle_textarea"></div><div id="autocomplete"></div></div>',
-  props: ['initial', 'complete'],
+  props: ['initial', 'value'],
   mounted: function() {
+    var component = this;
+
     this.tag_editor = new Taggle('taggle-editor', {
       preserveCase: true,
       tags: this.initial,
+      onTagAdd: function() {
+        component.$emit('input', '')
+      }
     })
 
-    var component = this;
     $('#tag-editor-submit').click(function() {
       var tags = component.tag_editor.getTags().values
       component.$emit('submit', tags)
     })
 
-    // Add autocomplete
-    var container = $('#autocomplete')
-    console.log(container)
+    // Attach events, used by autocomplete
     var input = this.tag_editor.getInput()
-
-    var editor = this.tag_editor
-    var autocomplete_tags = this.complete
-    $(input).autocomplete({
-      source: autocomplete_tags,
-      appendTo: container,
-      position: {at: 'left bottom', of: container},
-      select: function(event, data) {
-        event.preventDefault()
-        editor.add(data.item.value)
-      }
+    $(input).on('input', function(e) {
+      component.$emit('input', $(e.target).val())
     })
+
+    $(input).on('focus', function(e) {
+      component.$emit('focus')
+    })
+
+    $(input).on('blur', function(e) {
+      component.$emit('input', '')
+      component.$emit('blur')
+    })
+  },
+  watch: {
+    value: function() {
+      this.tag_editor.add(this.value)
+    }
   }
 }
 
@@ -63,7 +70,8 @@ var vm = new Vue({
     'taggle-tags': tagComponent
   },
   data: {
-    saved_tags: []
+    saved_tags: [],
+    selected: '',
   },
   created: function() {
     var tags = JSON.parse($('#tags-json').text())
@@ -90,6 +98,12 @@ var vm = new Vue({
 
       this.saved_tags = tags
       $('#TagEditorModal').modal('hide')
+    },
+    getSuggestions: function() {
+      return this.user_tags
+    },
+    makeSuggestion: function(suggestion) {
+      this.selected = suggestion
     }
   }
 })
