266dd1e381382ecee2660e2745f95c694bde5c5f
Harry White <harry.white424@gmail.com>
2018-09-18 10:03:32 +1000

    Started Component combining

--- python/aristotle-metadata-registry/aristotle_mdr/static/aristotle_mdr/itemPage.js
+++ python/aristotle-metadata-registry/aristotle_mdr/static/aristotle_mdr/itemPage.js
@@ -109,6 +109,49 @@ tagComponent = {
   }
 }
 
+var autoCompleteTagComponent = {
+  template: '<vue-simple-suggest :list="getSuggestions" :filter-by-query="true" @select="makeSuggestion">\
+    <taggle-tags :tags="current_tags" :newtags="newTags" @tag-update="update_tags"></taggle-tags>\
+  </vue-simple-suggest>',
+  components: {
+    'taggle-tags': tagComponent,
+  },
+  props: ['value', 'user_tags'],
+  computed: {
+    newTags: function() {
+      newTags = []
+      for (var i=0; i < this.value.length; i++) {
+        var element = this.current_tags[i]
+        if (this.user_tags.indexOf(element) == -1) {
+          newTags.push(element)
+        }
+      }
+      return newTags
+    }
+  },
+  methods: {
+    getSuggestions: function() {
+      suggestions = []
+      for (var i=0; i < this.user_tags.length; i++) {
+        var element = this.user_tags[i]
+        // Add to suggestions if not in current tags
+        if (this.current_tags.indexOf(element) == -1) {
+          suggestions.push(element)
+        }
+      }
+      return suggestions
+    },
+    makeSuggestion: function(suggestion) {
+      if (suggestion != null) {
+        this.current_tags.push(suggestion)
+      }
+    },
+    update_tags: function(tags) {
+      this.current_tags = tags
+    },
+  }
+}
+
 var simpleList = {
   template: '<ul :class="ulClass">\
     <li v-for="item in data" :class="liClass">{{ item }}</li>\
