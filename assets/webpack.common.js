f982374bc44759118e621d850483ea953590947b
Harry White <harry.white424@gmail.com>
2018-09-21 15:03:16 +1000

    Expose jquery

--- assets/webpack.common.js
+++ assets/webpack.common.js
@@ -9,8 +9,22 @@ module.exports = {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist/bundles'),
   },
+  module: {
+    rules: [{
+      test: require.resolve('jquery'),
+      use: [{
+        // Expose jquery outside bundle
+        loader: 'expose-loader',
+        options: '$'
+      },{
+        loader: 'expose-loader',
+        options: 'jQuery'
+      }]
+    }]
+  },
   plugins: [
     new webpack.ProvidePlugin({
+      // Provide $ and jQuery to scripts, no need to import
       $: "jquery",
       jQuery: "jquery"
     })
