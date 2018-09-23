6fe55733a922e6b4a509aa9c0766d7bdb5a7c025
Harry White <harry.white424@gmail.com>
2018-09-24 09:45:17 +1000

    Added messages lib

---
+++ assets/src/lib/messages.js
@@ -0,0 +1,21 @@
+function clearMessages() {
+  var message_list = $('#messages-row').find('ul').html('')
+}
+
+function clearAndHideMessages() {
+  clearMessages()
+  var row = $('#messages-row').attr('hidden', '')
+}
+
+export function addHeaderMessage(message) {
+  clearMessages()
+  var row = $('#messages-row')
+  row.find('.alert').removeAttr('hidden')
+  var message_list = row.find('ul')
+  var element = $('<li>', {text: message})
+  message_list.append(element)
+}
+
+export function initMessages() {
+  $('#messages-row').find('.close').click(clearAndHideMessages)
+}
