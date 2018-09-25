5835749eefd342159f846065ab4cf8dcf9f9b0ff
Sam Spencer <theodore.therone@gmail.com>
2018-09-25 09:04:18 +0000

    fix bad json

--- docker/sh/entrypoint/web.sh
+++ docker/sh/entrypoint/web.sh
@@ -1,2 +1,5 @@
 #tail -f /dev/null # If things go wrong use this to get the container up
+set -e # Errors will cause the script to stop
+pip install django-impersonate psycopg2 django-improved-user==1.0.0
+django-admin check
 gunicorn wsgi:application -b 0.0.0.0:8000 -w 2 --log-file=- --access-logfile=- --log-level debug --reload
