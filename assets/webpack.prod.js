146853fb4c6fc2b4dc1947cff4ec66bcbf517d1f
Harry White <harry.white424@gmail.com>
2018-09-27 09:41:03 +1000

    Minimize css in production

--- assets/webpack.prod.js
+++ assets/webpack.prod.js
@@ -1,6 +1,15 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+
 module.exports = merge(common, {
-  mode: 'production'
+  mode: 'production',
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin(),
+      new OptimizeCSSAssetsPlugin()
+    ]
+  }
 })
