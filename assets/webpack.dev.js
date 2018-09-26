9fad1a0e7318d6c591944e96eb6d240fa49433cd
Harry White <harry.white424@gmail.com>
2018-09-26 11:30:56 +1000

    Add webpack bundle analyzer

--- assets/webpack.dev.js
+++ assets/webpack.dev.js
@@ -1,10 +1,16 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   watchOptions: {
     ignored: /node_modules/
-  }
+  },
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static'
+    })
+  ]
 })
