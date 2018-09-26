d8fcf992038078d940acb14a7c2c7d83514c2af0
Harry White <harry.white424@gmail.com>
2018-09-26 11:12:48 +1000

    Add dashboard and wizard pages, integrate dashboard

---
+++ assets/src/pages/dashboard.js
@@ -0,0 +1,6 @@
+// May be used on several pages
+import init from '../lib/init.js'
+
+import '../styles/aristotle.dashboard.less'
+
+init(true)
---
+++ assets/src/pages/wizard.js
@@ -0,0 +1,6 @@
+// Your a wizard harry
+import init from '../lib/init.js'
+
+import '../styles/aristotle.wizard.less'
+
+init(true)
