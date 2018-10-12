aad8b9e9386392ca68c73626f6fad8b84cb0bd80
Harry White <harry.white424@gmail.com>
2018-10-12 14:54:50 +1100

    cherry :cherry:

--- python/aristotle-metadata-registry/aristotle_mdr/tests/main/test_notifications.py
+++ python/aristotle-metadata-registry/aristotle_mdr/tests/main/test_notifications.py
@@ -39,9 +39,9 @@ class TestNotifications(utils.LoggedInViewPages, TestCase):
 
     def test_subscriber_is_notified_of_supersede(self):
         user1 = get_user_model().objects.create_user('subscriber@example.com','subscriber')
-        user1.profile.favourites.add(self.item1)
         self.wg1.viewers.add(user1)
-        self.assertTrue(user1.profile in self.item1.favourited_by.all())
+        self.favourite_item(user1, self.item1)
+        self.assertTrue(user1 in self.item1.favourited_by.all())
 
         self.assertEqual(user1.notifications.all().count(), 0)
         self.assertTrue(self.item1.can_view(user1))
@@ -60,9 +60,9 @@ class TestNotifications(utils.LoggedInViewPages, TestCase):
 
     def test_subscriber_is_not_notified_of_supersedes_on_invisible_items(self):
         user1 = get_user_model().objects.create_user('subscriber@example.com','subscriber')
-        user1.profile.favourites.add(self.item1)
         self.wg1.viewers.add(user1)
-        self.assertTrue(user1.profile in self.item1.favourited_by.all())
+        self.favourite_item(user1, self.item1)
+        self.assertTrue(user1 in self.item1.favourited_by.all())
 
         self.assertEqual(user1.notifications.all().count(), 0)
         self.assertTrue(self.item1.can_view(user1))
