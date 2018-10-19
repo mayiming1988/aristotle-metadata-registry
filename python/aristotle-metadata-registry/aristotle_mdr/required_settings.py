22ce8d6b429b49973e656e59253bf0a131652eeb
Harry White <harry.white424@gmail.com>
2018-10-19 11:21:38 +1100

    Add header tags to allowed

--- python/aristotle-metadata-registry/aristotle_mdr/required_settings.py
+++ python/aristotle-metadata-registry/aristotle_mdr/required_settings.py
@@ -279,7 +279,8 @@ CACHE_ITEM_PAGE = False
 # Sanitization
 BLEACH_ALLOWED_TAGS = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code', 'em',
                        'i', 'li', 'ol', 'strong', 'ul', 'table', 'tbody', 'thead',
-                       'tr', 'th', 'td', 'img', 'p']
+                       'tr', 'th', 'td', 'img', 'p', 'h1', 'h2', 'h3', 'h4',
+                       'h5', 'h6']
 
 BLEACH_ALLOWED_ATTRIBUTES = {
     'a': ['href', 'title'],
