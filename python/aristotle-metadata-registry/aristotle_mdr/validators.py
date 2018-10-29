72ed7213fcf0898e1d858be1e43588c5e12bf793
Harry White <harry.white424@gmail.com>
2018-10-29 12:37:42 +1100

    Update validators

--- python/aristotle-metadata-registry/aristotle_mdr/validators.py
+++ python/aristotle-metadata-registry/aristotle_mdr/validators.py
@@ -6,13 +6,20 @@ class BaseValidator:
     def __init__(self, rule):
         self.rule = rule
 
-    def validate(self, field_data):
+    def validate(self, item):
         # To be overwritten in child
         raise NotImplementedError
 
 
 class RegexValidator(BaseValidator):
 
-    def validate(self, field_data):
-        match = re.fullmatch(self.rule, field_data)
-        return (match is not None)
+    def validate(self, item):
+        field_data = getattr(item, self.rule['field'])
+        match = re.fullmatch(self.rule['regex'], field_data)
+        return (match is not None), ''
+
+
+class StatusValidator(BaseValidator):
+
+    def validate(self, item):
+        return True, ''
