71a88cf240fd654206f623176da5a137a2b12bcc
Harry White <harry.white424@gmail.com>
2018-11-05 14:25:38 +1100

    Add bsFormField component

---
+++ assets/src/components/bsFormField.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="form-group">
+    <label :for="name">{{ capitalize(name) }}</label>
+    <slot :id="name"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['name'],
+    methods: {
+        capitalize: (name) => {
+            let first = name.charAt(0)
+            return first.toUpperCase() + name.slice(1)
+        }
+    }
+}
+</script>
--- assets/src/components/issueModal.vue
+++ assets/src/components/issueModal.vue
@@ -1,22 +1,39 @@
 <template>
   <modal :value="value" title="Create Issue" @input="emitClose">
-    <issue-create></issue-create> 
+    <form-field name="name">
+      <input id="name" class="form-control" v-model="name" />
+    </form-field>
+    <form-field name="description">
+      <textarea id="description" class="form-control" v-model="description" />
+    </form-field>
+    <div slot="footer">
+      <button type="button" class="btn btn-default" @click="emitClose">Close</button>
+      <button type="button" class="btn btn-primary" @click="createIssue">Create Issue</button>
+    </div>
   </modal>
 </template>
 
 <script>
 import { Modal } from 'uiv'
-import IssueCreate from '../components/issueCreate.vue'
+import formField from '../components/bsFormField.vue'
 
 export default {
     components: {
         Modal,
-        IssueCreate
+        formField
     },
-    props: ['value'],
+    props: ['iid', 'value'],
+    data: () => ({
+        name: '',
+        description: ''
+    }),
     methods: {
         emitClose: function() {
             this.$emit('input', false)
+        },
+        createIssue: function() {
+            console.log('created')
+            this.emitClose()
         }
     }
 }
