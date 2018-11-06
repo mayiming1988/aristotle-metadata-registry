06065a16b8e79bcdb6bd89e525e1f4d630fc7912
Harry White <harry.white424@gmail.com>
2018-11-06 17:21:16 +1100

    Some more permissions tests

--- python/aristotle-mdr-api/aristotle_mdr_api/v4/tests.py
+++ python/aristotle-mdr-api/aristotle_mdr_api/v4/tests.py
@@ -16,11 +16,21 @@ class BaseAPITestCase(TestCase):
             email='testuser@example.com',
             password='testing123'
         )
+        self.other_user = self.um.objects.create_user(
+            email='anothertestuser@example.com',
+            password='1234'
+        )
 
     def login_user(self):
         self.client.login(
             email='testuser@example.com',
             password='testing123'
+        )
+
+    def login_other_user(self):
+        self.client.login(
+            email=self.other_user.email,
+            password='1234'
         )
 
 
@@ -90,3 +100,58 @@ class IssueEndpointsTestCase(BaseAPITestCase):
 
         comments = issue.comments.all()
         self.assertEqual(len(comments), 1)
+
+    @tag('issue_comments')
+    def test_cant_comment_non_viewable_issue(self):
+        issue = models.Issue.objects.create(
+            name='Many problem',
+            description='many',
+            item=self.item,
+            submitter=self.user,
+        )
+
+        self.login_other_user()
+        response = self.client.post(
+            reverse('api_v4:issue_comment'),
+            {
+                'body': 'Test comment',
+                'issue': issue.id
+            },
+            format='json'
+        )
+        self.assertEqual(response.status_code, 400)
+        self.assertTrue('issue' in response.data)
+
+
+@tag('perms')
+class PermsTestCase(BaseAPITestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.item = mdr_models.ObjectClass.objects.create(
+            name='Brand new item',
+            definition='Great'
+        )
+        self.issue = models.Issue.objects.create(
+            name='Many problem',
+            description='many',
+            item=self.item,
+            submitter=self.user
+        )
+
+    def test_get_issue_allowed(self):
+
+        self.login_user()
+        response = self.client.get(
+            reverse('api_v4:issues', args=[self.issue.pk]),
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_issue_not_allowed(self):
+
+        self.login_other_user()
+        response = self.client.get(
+            reverse('api_v4:issues', args=[self.issue.pk]),
+        )
+        self.assertEqual(response.status_code, 403)
+
