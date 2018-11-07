e0e4da7d79c0f6c784027a40f71a98765f187477
Sam Spencer <theodore.therone@gmail.com>
2018-11-07 08:11:16 +0000

    add extra validation to forms

--- python/aristotle-metadata-registry/aristotle_mdr/contrib/publishing/forms.py
+++ python/aristotle-metadata-registry/aristotle_mdr/contrib/publishing/forms.py
@@ -1,8 +1,10 @@
+from django import forms
+from django.core.exceptions import ValidationError
 from django.forms import ModelForm
+
 from .models import VersionPublicationRecord
 from aristotle_mdr.widgets.bootstrap import BootstrapDateTimePicker
-from django.forms import RadioSelect
-from django import forms
+
 
 class MetadataPublishForm(ModelForm):
     notes = forms.TextInput()
@@ -18,3 +20,12 @@ class MetadataPublishForm(ModelForm):
                 options={"format": "YYYY-MM-DD HH:MM"}
             ),
         }
+
+    def clean(self):
+        pub_date = self.cleaned_data['public_user_publication_date']
+        priv_date = self.cleaned_data['authenticated_user_publication_date']
+
+        if (pub_date and priv_date) and priv_date > pub_date:
+            raise ValidationError(
+                "Authenticated user publication date cannot be after public user publication date."
+            )
