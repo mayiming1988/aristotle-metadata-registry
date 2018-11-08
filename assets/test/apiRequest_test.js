822b07a823b4221bc6d46963be6a355becd8c7f5
Harry White <harry.white424@gmail.com>
2018-11-08 18:47:35 +1100

    First api request test

--- assets/test/apiRequest_test.js
+++ assets/test/apiRequest_test.js
@@ -2,7 +2,7 @@ import chai from 'chai'
 import sinon from 'sinon'
 import VueTestUtils from '@vue/test-utils'
 
-import { apiRequest } from '../src/mixins/apiRequest.js'
+import apiRequest from '../src/mixins/apiRequest.js'
 import switchEditApi from '../src/components/switchEditApi.vue'
 
 var assert = chai.assert
@@ -12,25 +12,34 @@ var mount = VueTestUtils.mount
 
 describe('apiRequest', function() {
 
-
     beforeEach(function() {
-        var testApiComponent = {
-            template: '<div>hello<\/div>',
-            mixins: [apiRequest]
-        }
-        let wrapper = shallowMount(testApiComponent, {})
-
-        this.server = sinon.createFakeServer()
+        this.wrapper = shallowMount(apiRequest)
+        this.server = sinon.createFakeServer({autoRespond: true})
+        document.cookie = 'csrftoken=faketoken'
     })
 
     afterEach(function() {
         this.server.restore()
+        this.wrapper = {}
+        document.cookie = ''
     })
 
-    it('makes get request', function() {
-        wrapper.vm.get('/fake/api/')
-        this.server.respond()
+    it('makes get request', function(done) {
+        this.server.respondWith([
+            200,
+            {'Content-Type': 'application/json'},
+            JSON.stringify({some: 'data'})
+        ])
+        let promise = this.wrapper.vm.get('/fake/api/')
 
-        assert.equal(this.server.requests.length, 1)
+        promise.then((response) => {
+            console.log('we in')
+            assert.equal(this.server.requests.length, 1)
+            let request = this.server.requests[0]
+            assert.equal(request.method, 'GET')
+
+            assert.deepEqual(this.wrapper.vm.response.data, {some: 'data'})
+            done()
+        })
     })
 })
