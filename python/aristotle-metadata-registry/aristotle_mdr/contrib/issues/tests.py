c5ab8668b16202e40d3520c6bd1df5ef796d6494
Harry White <harry.white424@gmail.com>
2018-11-15 12:33:29 +1100

    More issues view tests

--- python/aristotle-metadata-registry/aristotle_mdr/contrib/issues/tests.py
+++ python/aristotle-metadata-registry/aristotle_mdr/contrib/issues/tests.py
@@ -14,9 +14,9 @@ class IssueTests(AristotleTestUtils, TestCase):
             workgroup=self.wg1
         )
 
-    def create_test_issue(self):
+    def create_test_issue(self, name='Test Issue'):
         return models.Issue.objects.create(
-            name='Test issue',
+            name=name,
             description='Just a test',
             item=self.item,
             submitter=self.editor
@@ -27,7 +27,7 @@ class IssueTests(AristotleTestUtils, TestCase):
         self.assertTrue(issue.isopen)
         self.assertIsNotNone(issue.created)
 
-    def test_issue_display(self):
+    def test_issue_displays(self):
         issue = self.create_test_issue()
         self.login_viewer()
         response = self.reverse_get(
@@ -39,4 +39,48 @@ class IssueTests(AristotleTestUtils, TestCase):
 
         issues = response.context['issues']
         self.assertEqual(len(issues), 1)
-        self.assertEqual(issues[0].name, 'Test issue')
+        self.assertEqual(issues[0].name, 'Test Issue')
+
+    def test_issues_list_open_closed(self):
+        openis = self.create_test_issue()
+        closedis = self.create_test_issue()
+        closedis.isopen = False
+        closedis.save()
+
+        self.login_viewer()
+        response = self.reverse_get(
+            'aristotle_issues:item_issues',
+            reverse_args=[self.item.id],
+            status_code=200
+        )
+        context = response.context
+
+        self.assertEqual(len(context['open_issues']), 1)
+        self.assertEqual(len(context['closed_issues']), 1)
+
+        self.assertEqual(context['open_issues'][0].id, openis.id)
+        self.assertEqual(context['closed_issues'][0].id, closedis.id)
+
+    def test_own_issue_true_on_own_issue(self):
+        issue = self.create_test_issue()
+        self.login_editor()
+
+        response = self.reverse_get(
+            'aristotle_issues:issue',
+            reverse_args=[self.item.id, issue.pk],
+            status_code=200
+        )
+
+        self.assertTrue(response.context['own_issue'])
+
+    def test_own_issue_false_on_othes_issue(self):
+        issue = self.create_test_issue()
+        self.login_viewer()
+
+        response = self.reverse_get(
+            'aristotle_issues:issue',
+            reverse_args=[self.item.id, issue.pk],
+            status_code=200
+        )
+
+        self.assertFalse(response.context['own_issue'])
