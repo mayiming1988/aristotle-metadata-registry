4cca3e0b0207c84a4f154a90e86285f656ad5575
Harry White <harry.white424@gmail.com>
2018-11-20 16:44:17 +1100

    Test for root item migration

--- python/aristotle-metadata-registry/aristotle_mdr/contrib/links/migrations/0006_link_root_item.py
+++ python/aristotle-metadata-registry/aristotle_mdr/contrib/links/migrations/0006_link_root_item.py
@@ -7,18 +7,6 @@ from django.db import migrations
 import django.db.models.deletion
 
 
-def forward_add_root_item(apps, schema_editor):
-    link_model = apps.get_model('aristotle_mdr_links', 'Link')
-    for link in link_model.objects.all():
-        first_le = link.linkend_set.order_by('created').select_related('concept').first()
-        link.root_item = first_le.concept
-        link.save()
-
-
-def backward_add_root_item(apps, schema_editor):
-    print('Root items will be lost')
-
-
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -32,10 +20,4 @@ class Migration(migrations.Migration):
             name='root_item',
             field=aristotle_mdr.fields.ConceptForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='owned_links', to='aristotle_mdr._concept'),
         ),
-        migrations.RunPython(forward_add_root_item, backward_add_root_item),
-        migrations.AlterField(
-            model_name='link',
-            name='root_item',
-            field=aristotle_mdr.fields.ConceptForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='owned_links', to='aristotle_mdr._concept'),
-        )
     ]
