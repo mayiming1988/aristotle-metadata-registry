ce9032ed64fca2a42d445e57d58ecfbf9f52bc0c
Harry White <harry.white424@gmail.com>
2018-11-28 10:03:40 +1100

    Add missing slash

--- python/aristotle-mdr-api/aristotle_mdr_api/v4/urls.py
+++ python/aristotle-mdr-api/aristotle_mdr_api/v4/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 from aristotle_mdr_api.v4 import views
 
 urlpatterns = [
-    url(r'item', include('aristotle_mdr_api.v4.concepts.urls')),
+    url(r'item/', include('aristotle_mdr_api.v4.concepts.urls')),
     url(r'issues/$', views.IssueCreateView.as_view(), name='issues_create'),
     url(r'issues/(?P<pk>\d+)/$', views.IssueView.as_view(), name='issues'),
     url(r'issues/(?P<pk>\d+)/updatecomment/$', views.IssueUpdateAndCommentView.as_view(), name='issue_update_and_comment'),
