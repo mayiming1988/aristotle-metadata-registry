358b0cf69f53b1169994f4d7df33c4e598aa4793
Harry White <harry.white424@gmail.com>
2018-12-10 14:33:36 +1100

    Test for add and remove rows

--- assets/test/forms/formset_test.js
+++ assets/test/forms/formset_test.js
@@ -7,6 +7,8 @@ import Formset from '@/forms/formSet.vue'
 
 describe('FormSet', function() {
 
+    let ntfields = {name: {rules: {required: true}}, type: {rules: {required: false}}}
+
     function getWrapper(initial, fields) {
         let propsData = {fields: fields, initial: initial}
         return VueTestUtils.shallowMount(Formset, {propsData: propsData})
@@ -33,14 +35,35 @@ describe('FormSet', function() {
     })
 
     it('Maps backend errors to vid', function() {
-        let fields = {name: {rules: {required: true}}, type: {rules: {required: false}}}
         let initial = [{name: 'Grape', type: 'Fruit'}, {name: 'lmao', type: 'Abbreviation'}]
-        let wrapper = getWrapper(initial, fields)
+        let wrapper = getWrapper(initial, ntfields)
         wrapper.setProps({
             errors: [{name: ['Required']}, {type: ['That type is bad']}]
         })
 
         let expectedMap = {0: {name: ['Required']}, 1: {type: ['That type is bad']}}
         assert.deepEqual(wrapper.vm.error_map, expectedMap)
+    })
+    
+    it('adds rows', function() {
+        let fields = {
+            name: {rules: {required: true}, default: 'MyName'}, 
+            type: {rules: {required: false}}
+        }
+        let wrapper = getWrapper([], fields)
+
+        assert.equal(wrapper.vm.formsData.length, 0)
+        wrapper.vm.addRow()
+        assert.deepEqual(wrapper.vm.formsData, [{name: 'MyName', vid: 0, new: true}])
+    })
+
+    it('deletes row', function() {
+        let initial = [{name: 'Yes', type: 'Boolean'}, {name: 'No', type: 'Boolean'}]
+        let wrapper = getWrapper(initial, ntfields)
+
+        wrapper.vm.deleteRow(1)
+
+        let expectedData = [{name: 'Yes', type: 'Boolean', vid: 0, new: false}]
+        assert.deepEqual(wrapper.vm.formsData, expectedData)
     })
 })
