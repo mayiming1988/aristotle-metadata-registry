a4d1f9bf8c2634475cdb0573ab489f28d2c19f21
Harry White <harry.white424@gmail.com>
2018-12-11 14:15:23 +1100

    Reorder urls

--- python/aristotle-mdr-api/aristotle_mdr_api/v4/urls.py
+++ python/aristotle-mdr-api/aristotle_mdr_api/v4/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url, include
 from aristotle_mdr_api.v4 import views
 
 urlpatterns = [
-    url(r'item/', include('aristotle_mdr_api.v4.concepts.urls')),
     url(r'tags/', include('aristotle_mdr_api.v4.tags.urls')),
+    url(r'item/', include('aristotle_mdr_api.v4.concepts.urls')),
     url(r'issues/$', views.IssueCreateView.as_view(), name='issues_create'),
     url(r'issues/(?P<pk>\d+)/$', views.IssueView.as_view(), name='issues'),
     url(r'issues/(?P<pk>\d+)/updatecomment/$', views.IssueUpdateAndCommentView.as_view(), name='issue_update_and_comment'),
