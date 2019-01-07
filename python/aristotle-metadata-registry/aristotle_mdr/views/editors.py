7860f7afe448e0a6c372c664fd9b1b4fb2887333
Harry White <harry.white424@gmail.com>
2019-01-07 15:08:51 +1100

    Update component creator to deal with accessor names vs field names

--- python/aristotle-metadata-registry/aristotle_mdr/views/editors.py
+++ python/aristotle-metadata-registry/aristotle_mdr/views/editors.py
@@ -224,7 +224,7 @@ class CloneItemView(ConceptEditFormView, SingleObjectMixin, FormView):
         for field_name in fields:
             field = self.model._meta.get_field(field_name)
             remote_field_name = field.remote_field.name
-            components = getattr(original, field_name).all()
+            components = getattr(original, field.get_accessor_name()).all()
             new_components = []
             for component in components:
                 # Set pk to none so we insert instead of update
