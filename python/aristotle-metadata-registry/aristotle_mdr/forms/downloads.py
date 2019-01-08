22824f1ddee5c667ec5fff900f886fb5b1d593a7
Harry White <harry.white424@gmail.com>
2019-01-08 12:11:35 +1100

    Options not required

--- python/aristotle-metadata-registry/aristotle_mdr/forms/downloads.py
+++ python/aristotle-metadata-registry/aristotle_mdr/forms/downloads.py
@@ -3,5 +3,5 @@ from django import forms
 
 class DownloadOptionsForm(forms.Form):
 
-    include_supporting = forms.BooleanField()
-    email_copy = forms.BooleanField()
+    include_supporting = forms.BooleanField(required=False)
+    email_copy = forms.BooleanField(required=False)
