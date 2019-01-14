24b6656e065eed3f68955e1333fcb764f87f5945
Harry White <harry.white424@gmail.com>
2019-01-15 09:15:05 +1100

    Stylefix

--- python/aristotle-metadata-registry/aristotle_mdr/apps.py
+++ python/aristotle-metadata-registry/aristotle_mdr/apps.py
@@ -4,6 +4,7 @@ from django.apps import AppConfig
 class AristotleExtensionBaseConfig(AppConfig):
     pass
 
+
 class AristotleMDRConfig(AristotleExtensionBaseConfig):
     name = 'aristotle_mdr'
     verbose_name = "Aristotle Metadata Registry"
