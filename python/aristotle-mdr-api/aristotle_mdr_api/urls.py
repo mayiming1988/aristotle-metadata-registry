299760e379a767b14521ae309a7f4d0352f65ff8
Harry White <harry.white424@gmail.com>
2019-01-21 13:34:57 +1100

    Move schema views, add manual schema for item tags

--- python/aristotle-mdr-api/aristotle_mdr_api/urls.py
+++ python/aristotle-mdr-api/aristotle_mdr_api/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from rest_framework_swagger.views import get_swagger_view
+from rest_framework.schemas import get_schema_view
 from django.utils.module_loading import import_string
 from .views import APIRootView
 # from rest_framework.authtoken import views as tokenviews
@@ -14,25 +15,13 @@ machine-readable interface.
 
 """
 
-def version_schema(*args, **kwargs):
-    version = kwargs.pop('version')
-    if version:
-        patterns = import_string('aristotle_mdr_api.%s.urls.urlpatterns' % version)
-    else:
-        patterns = []
-
-    return get_swagger_view(
-            title='Aristotle API %s' % version,
-            patterns=patterns
-        )(*args)
-
 
 urlpatterns = [
     url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
     # url(r'^api-token-auth/', tokenviews.obtain_auth_token),
     url(r'^token/', include('aristotle_mdr_api.token_auth.urls', namespace='token_auth')),
-    url(r'^(?P<version>(v3|v4)?)/schemas/', version_schema),
-    url(r'^schemas/', get_swagger_view(title='Aristotle API')),
+    url(r'^v3/schemas/', get_swagger_view(title='Aristotle v3 API', urlconf='aristotle_mdr_api.v3.urls'), name='schema_v3'),
+    url(r'^v4/schemas/', get_swagger_view(title='Aristotle v4 API', urlconf='aristotle_mdr_api.v4.urls'), name='schema_v4'),
     url(r'^$', APIRootView.as_view(), name="aristotle_api_root"),
 
     url(r'^v3/', include('aristotle_mdr_api.v3.urls', namespace='aristotle_mdr_api.v3')),
--- python/aristotle-mdr-api/aristotle_mdr_api/v4/urls.py
+++ python/aristotle-mdr-api/aristotle_mdr_api/v4/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import include, url
+from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
     url(r'^custom_fields/', include('aristotle_mdr_api.v4.custom_fields.urls')),
@@ -6,4 +7,5 @@ urlpatterns = [
     url(r'^item/', include('aristotle_mdr_api.v4.concepts.urls')),
     url(r'^reviews/', include('aristotle_mdr_api.v4.reviews.urls', namespace='reviews')),
     url(r'^tags/', include('aristotle_mdr_api.v4.tags.urls')),
+    url(r'^$', get_schema_view(title='Aristotle v4 API', urlconf='aristotle_mdr_api.v4.urls')),
 ]
