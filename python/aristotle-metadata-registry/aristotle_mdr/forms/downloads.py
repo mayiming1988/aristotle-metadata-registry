08eeeedef4fd487a29b6501eb02ab34f668565a9
Harry White <harry.white424@gmail.com>
2019-01-23 12:53:34 +1100

    Add help text

--- python/aristotle-metadata-registry/aristotle_mdr/forms/downloads.py
+++ python/aristotle-metadata-registry/aristotle_mdr/forms/downloads.py
@@ -10,7 +10,19 @@ class DownloadOptionsForm(forms.Form):
             self.fields['back_page'] = forms.FileField(required=False)
         self.wrap_pages = wrap_pages
 
-    title = forms.CharField(required=False)
-    include_supporting = forms.BooleanField(required=False)
-    include_related = forms.BooleanField(required=False)
-    email_copy = forms.BooleanField(required=False)
+    title = forms.CharField(
+        required=False,
+        help_text='The title of the document'
+    )
+    include_supporting = forms.BooleanField(
+        required=False,
+        help_text='Include the name and definition for components of the item (e.g. the Value Domain of a Data Element)'
+    )
+    include_related = forms.BooleanField(
+        required=False,
+        help_text='Include the name and definition of all concepts implementing the item (e.g. the Data Elements using a Value Domain)'
+    )
+    email_copy = forms.BooleanField(
+        required=False,
+        help_text='Send a copy of the download to your email address'
+    )
