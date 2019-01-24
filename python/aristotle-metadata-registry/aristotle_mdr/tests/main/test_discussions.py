ad49a4bec9e377b8e6dc7febe92281c2d6748fa8
Sam Spencer <theodore.therone@gmail.com>
2019-01-24 21:18:50 +0000

    syntax fix

--- python/aristotle-metadata-registry/aristotle_mdr/tests/main/test_discussions.py
+++ python/aristotle-metadata-registry/aristotle_mdr/tests/main/test_discussions.py
@@ -16,8 +16,8 @@ class PostingAndCommentingAtObjectLevel(TestCase):
 
     def setUp(self):
         self.steward_org = models.StewardOrganisation.objects.create(name="Test SO")
-        self.wg1 = models.Workgroup.objects.create(name="Test WG 1", stewardship_organisation=self.steward_org))
-        self.wg2 = models.Workgroup.objects.create(name="Test WG 2", stewardship_organisation=self.steward_org))
+        self.wg1 = models.Workgroup.objects.create(name="Test WG 1", stewardship_organisation=self.steward_org)
+        self.wg2 = models.Workgroup.objects.create(name="Test WG 2", stewardship_organisation=self.steward_org)
         self.viewer1 = get_user_model().objects.create_user('vicky@example.com','viewer') # viewer 1 always posts
         self.viewer2 = get_user_model().objects.create_user('viewer2@example.com','viewer')
         self.manager = get_user_model().objects.create_user('mandy@example.com','manger')
@@ -78,7 +78,7 @@ class WorkgroupMembersCanMakePostsAndComments(utils.LoggedInViewPages,TestCase):
         self.viewer2 = get_user_model().objects.create_user('viewer2@example.com','viewer') # not in any workgroup
         self.viewer3 = get_user_model().objects.create_user('viewer3@example.com','viewer') # not in our "primary testing workgroup" (self.wg1)
         self.wg1.giveRoleToUser('viewer',self.viewer3)
-        self.wg2 = models.Workgroup.objects.create(name="Test WG 2", stewardship_organisation=self.steward_org_1))
+        self.wg2 = models.Workgroup.objects.create(name="Test WG 2", stewardship_organisation=self.steward_org_1)
 
     def can_the_current_logged_in_user_toggle_post(self):
         post = models.DiscussionPost.objects.create(author=self.viewer,workgroup=self.wg1,title="test",body="test")
@@ -500,7 +500,7 @@ class ViewDiscussionPostPage(utils.LoggedInViewPages,TestCase):
 
     def test_nonmember_cannot_see_posts(self):
         self.login_viewer()
-        self.wg3 = models.Workgroup.objects.create(name="Test WG 3", stewardship_organisation=self.steward_org_1))
+        self.wg3 = models.Workgroup.objects.create(name="Test WG 3", stewardship_organisation=self.steward_org_1)
 
         response = self.client.get(reverse('aristotle:discussionsWorkgroup',args=[self.wg3.id]))
         self.assertEqual(response.status_code,403)
