66cc27bbe4ea89513c40bdef0dc2929875916c5e
Harry White <harry.white424@gmail.com>
2019-02-07 09:49:32 +1100

    Rule validation in serializer

--- python/aristotle-mdr-api/aristotle_mdr_api/v4/rules/serializers.py
+++ python/aristotle-mdr-api/aristotle_mdr_api/v4/rules/serializers.py
@@ -1,15 +1,35 @@
 from rest_framework import serializers
 from aristotle_mdr.contrib.validators import models
+from aristotle_mdr.contrib.validators.schema.load import load_schema
+
+import yaml
+import jsonschema
 
 
-class RegistryRuleSerializer(serializers.ModelSerializer):
+class ValidateRulesSerializer(serializers.ModelSerializer):
+
+    def validate_rules(self, value):
+        schema = load_schema()
+
+        try:
+            rules = yaml.safe_load(value)
+        except yaml.YAMLError as ye:
+            raise serializers.ValidationError(ye)
+
+        try:
+            jsonschema.validate(rules, schema)
+        except jsonschema.exceptions.ValidationError as ve:
+            raise serializers.ValidationError(ve)
+
+
+class RegistryRuleSerializer(ValidateRulesSerializer):
 
     class Meta:
         model = models.RegistryValidationRules
         fields = ('rules',)
 
 
-class RARuleSerializer(serializers.ModelSerializer):
+class RARuleSerializer(ValidateRulesSerializer):
 
     class Meta:
         model = models.RAValidationRules
