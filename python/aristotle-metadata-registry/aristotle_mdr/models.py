7991ef027714698a02454b0ac1b0cbb2cbbc3c4d
Harry White <harry.white424@gmail.com>
2019-02-13 10:07:57 +1100

    Add manager to DedThrough

--- python/aristotle-metadata-registry/aristotle_mdr/models.py
+++ python/aristotle-metadata-registry/aristotle_mdr/models.py
@@ -1460,6 +1460,7 @@ class DedBaseThrough(models.Model):
     data_element_derivation = models.ForeignKey(DataElementDerivation, on_delete=models.CASCADE)
     data_element = models.ForeignKey(DataElement, on_delete=models.CASCADE)
     order = models.PositiveSmallIntegerField("Position")
+    objects = UtilsManager()
 
     class Meta:
         abstract = True
