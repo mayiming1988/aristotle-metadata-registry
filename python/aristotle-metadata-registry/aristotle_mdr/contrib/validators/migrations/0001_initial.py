142e876f8a5e1b7fb96bc673a95e5ea7a14d6f55
Harry White <harry.white424@gmail.com>
2019-02-13 09:43:52 +1100

    Fix migration dependancy

--- python/aristotle-metadata-registry/aristotle_mdr/contrib/validators/migrations/0001_initial.py
+++ python/aristotle-metadata-registry/aristotle_mdr/contrib/validators/migrations/0001_initial.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('aristotle_mdr', '0056_auto_20190118_0520'),
+        ('aristotle_mdr', '0047_auto_20190206_0600'),
     ]
 
     operations = [
