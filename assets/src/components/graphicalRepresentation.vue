96e734ecb3b88863ed51233f61d1aeb63af3604c
Francisco <fragalcer@gmail.com>
2019-02-14 17:09:56 +1100

    Fixed some problems found by Travis CI.

--- assets/src/components/graphicalRepresentation.vue
+++ assets/src/components/graphicalRepresentation.vue
@@ -139,38 +139,6 @@
                     // })
 
                     network.on('hoverNode', function() {
-                        // console.log(properties)
-                        // var nodeID = properties.node;
-                        // if (nodeID) {
-                        //
-                        //     var sNodeLabel = this.body.nodes[nodeID].options.label
-                        //     var sToolTip = this.body.nodes[nodeID].options.title;
-                        //
-                        //     //use JQUERY to see where the canvas is on the page.
-                        //     var canvasPosition = $('.vis-network').position();
-                        //
-                        //     //the properties give x & y relative to the edge of the canvas, not to the whole document.
-                        //     var clickX = properties.pointer.DOM.x + canvasPosition.top;
-                        //     var clickY = properties.pointer.DOM.y + canvasPosition.left;
-                        //
-                        //     //make sure we have a valid div, either clear it or generate one.
-                        //     if ($('#cellBatchAttrPopUp').length) {
-                        //         $('div#cellBatchAttrPopUp').empty();
-                        //     }
-                        //     else {
-                        //         $('<div id="cellBatchAttrPopUp"></div>').click(function () {
-                        //             //clicking the popup hides it again.
-                        //             $(this).empty().hide();
-                        //         }).css('position','absolute').appendTo("body");
-                        //     }
-                        //
-                        //     // put the div over the node, display the tooltip and show it.
-                        //     $('div#cellBatchAttrPopUp').append(sNodeLabel)
-                        //         .append('<br/>')
-                        //         .append(sToolTip)
-                        //         .css('top', clickY).css('left', clickX)
-                        //         .show();
-                        // }
                         document.body.style.cursor = 'pointer'
 
                     });
@@ -202,24 +170,5 @@
 </script>
 
 <style scoped>
-    /*div#cellBatchAttrPopUp {*/
-        /*display: none;*/
-        /*position: absolute;*/
-        /*z-index: 2000;*/
-        /*padding: 4px 8px;*/
-        /*color: #333;*/
-        /*white-space: nowrap;*/
-        /*-moz-border-radius: 5px;*/
-        /*-webkit-border-radius: 5px;*/
-        /*border-radius: 5px;*/
-        /*-moz-box-shadow: 0px 0px 4px #222;*/
-        /*-webkit-box-shadow: 0px 0px 4px #222;*/
-        /*box-shadow: 0px 0px 4px #222;*/
-        /*background-image: -moz-linear-gradient(top, #eeeeee, #cccccc);*/
-        /*background-image: -webkit-gradient(linear,left top,left bottom,color-stop(0, #eeeeee),color-stop(1, #cccccc));*/
-        /*background-image: -webkit-linear-gradient(top, #eeeeee, #cccccc);*/
-        /*background-image: -moz-linear-gradient(top, #eeeeee, #cccccc);*/
-        /*background-image: -ms-linear-gradient(top, #eeeeee, #cccccc);*/
-        /*background-image: -o-linear-gradient(top, #eeeeee, #cccccc);*/
-    /*}*/
+
 </style>
