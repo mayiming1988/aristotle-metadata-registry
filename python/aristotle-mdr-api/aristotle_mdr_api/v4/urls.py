70d7378d838c6ac4d89b2238d299e8edc7ac9ae5
Francisco <fragalcer@gmail.com>
2019-02-19 17:41:51 +1100

    Fixed some issues found by Travis CI.

--- python/aristotle-mdr-api/aristotle_mdr_api/v4/urls.py
+++ python/aristotle-mdr-api/aristotle_mdr_api/v4/urls.py
@@ -4,7 +4,7 @@ from rest_framework_swagger.views import get_swagger_view
 urlpatterns = [
     url(r'^custom_fields/', include('aristotle_mdr_api.v4.custom_fields.urls')),
     url(r'^issues/', include('aristotle_mdr_api.v4.issues.urls', namespace='issues')),
-    url(r'^item/', include('aristotle_mdr_api.v4.concepts.urls',namespace='item')),
+    url(r'^item/', include('aristotle_mdr_api.v4.concepts.urls', namespace='item')),
     url(r'^reviews/', include('aristotle_mdr_api.v4.reviews.urls', namespace='reviews')),
     url(r'^tags/', include('aristotle_mdr_api.v4.tags.urls')),
     url(r'^rules/', include('aristotle_mdr_api.v4.rules.urls')),
