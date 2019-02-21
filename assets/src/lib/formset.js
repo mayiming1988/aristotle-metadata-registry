d2c11393d96b3bc468a148e21902959908d44289
Harry White <harry.white424@gmail.com>
2019-02-21 14:41:40 +1100

    Generalize addRow

--- assets/src/lib/formset.js
+++ assets/src/lib/formset.js
@@ -1,16 +1,16 @@
 import { initDALWidget } from './dal_simple_init.js'
 
 export function replacePrefix(element, num_forms) {
-    var name = $(element).attr('name')
-    var id = $(element).attr('id')
+    let name = $(element).attr('name')
+    let id = $(element).attr('id')
 
     if (name && name.includes('__prefix__')) {
-        var new_name = name.replace('__prefix__', num_forms)
+        let new_name = name.replace('__prefix__', num_forms)
         $(element).attr('name', new_name)
     }
 
     if (id && id.includes('__prefix__')) {
-        var new_id = id.replace('__prefix__', num_forms)
+        let new_id = id.replace('__prefix__', num_forms)
         $(element).attr('id', new_id)
     }
 
@@ -21,10 +21,13 @@ export function addRow(formid, row_selector) {
     let panelList = $('#' + formid);
     let formstage = $('.formstage#' + formid + ' ' + row_selector)
 
-    //Recreate the date time pickers
-    //Get options from the formstage
+    // Clone the formstage
+    let new_form = formstage.clone();
+
+    // Recreate the date time pickers
+    // Get options from the formstage
     if (formstage.find('.date').data('DateTimePicker')) {
-        var options = formstage.find('.date').data('DateTimePicker').options()
+        let options = formstage.find('.date').data('DateTimePicker').options()
         //Initialize all date time objects
         new_form.find('.date').each(function() {
             $(this).datetimepicker(options);
@@ -34,11 +37,23 @@ export function addRow(formid, row_selector) {
     // Remove redundant select2s (they'll be remade when reinserted into the node)
     formstage.find('span.select2.select2-container').remove();
 
-    let new_form = formstage.clone();
-
-    var all_rows = panelList.find(row_selector)
-    var num_forms = $(all_rows).length
-    new_form.insertAfter(all_rows.last());
+    let all_rows = panelList.find(row_selector)
+    let num_forms = all_rows.length
+    if (num_forms > 0) {
+        // If there are existing forms insert after last one
+        new_form.insertAfter(all_rows.last());
+    } else {
+        // If this is the first form
+        let add_buttons = panelList.find('.add_code_button')
+        if (add_buttons.length > 0) {
+            // If an add button is in the panel list insert before that
+            let button = add_buttons.first()
+            new_form.insertBefore(button)
+        } else {
+            // otherwise just append to the panel list
+            panelList.append(new_form)
+        }
+    }
 
     new_form.find(':input').each(function() {
         replacePrefix(this, num_forms)
@@ -49,7 +64,7 @@ export function addRow(formid, row_selector) {
     $(total_forms_identifier).val(num_forms+1);
 
     new_form.find('[data-autocomplete-light-function=select2]').each(function() {
-        var element = $(this);
+        let element = $(this);
         initDALWidget(element)
     })
 }
