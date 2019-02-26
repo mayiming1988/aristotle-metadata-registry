fe0d222eb899f1be87c56dd795d7152ae9659871
Harry White <harry.white424@gmail.com>
2019-02-26 12:08:30 +1100

    Fix tag page component loading

--- assets/src/pages/tagPage.js
+++ assets/src/pages/tagPage.js
@@ -1,10 +1,14 @@
 import renderComponents from 'src/lib/renderComponents.js'
 import { initCore } from 'src/lib/init.js'
 
-import switchEditComponent from '@/switchEdit.vue'
+import switchEditComponent from '@/switchEditApi.vue'
+import inlineEdit from '@/inlineEdit.vue'
 
 import 'src/styles/taggle.css'
 import 'src/styles/aristotle.dashboard.less'
 
 initCore()
-renderComponents({'switch-edit': switchEditComponent})
+renderComponents({
+    'switch-edit': switchEditComponent,
+    'inline-edit': inlineEdit
+})
