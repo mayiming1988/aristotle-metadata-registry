9789bcea7998b764c1793a04e73eb4353a52cf81
Harry White <harry.white424@gmail.com>
2019-03-05 14:51:01 +1100

    No need to inspect terms

--- assets/src/pages/rr_supersedes.js
+++ assets/src/pages/rr_supersedes.js
@@ -6,11 +6,7 @@ function get_url(element) {
     let id = element.attr('id')
     let newer_item_select = document.getElementById(id.replace('older', 'newer'))
     let url = element.attr('data-autocomplete-light-url')
-    return function(params) {
-        let terms = params.terms
-        if (terms === undefined) {
-            terms = ''
-        }
+    return function() {
         if (newer_item_select !== null) {
             let selected = newer_item_select.options[newer_item_select.selectedIndex]
             if (selected !== null && selected.hasAttribute('data-label')) {
