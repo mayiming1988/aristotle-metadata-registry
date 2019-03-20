4eeeb5cad7cad022c69d5d685cc24a689541e5b6
Francisco <fragalcer@gmail.com>
2019-03-21 10:21:13 +1100

    Uploaded profile pictures now can be bigger than 2.5 Mb.

--- python/aristotle-metadata-registry/aristotle_mdr/contrib/reviews/tests.py
+++ python/aristotle-metadata-registry/aristotle_mdr/contrib/reviews/tests.py
@@ -10,8 +10,6 @@ import aristotle_mdr.tests.utils as utils
 from aristotle_mdr import perms
 from aristotle_mdr.contrib.reviews import models
 from aristotle_mdr.contrib.reviews.const import REVIEW_STATES
-from django.forms import ValidationError
-
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
@@ -444,21 +442,16 @@ class ReviewRequestSupersedesTestCase(utils.AristotleTestUtils, TestCase):
     def test_newer_item_field_and_older_item_field_are_required(self):
         old1 = self.create_editor_item('Old Object', 'old')
         new1 = self.item
+        empty_newer_item_data = [{'older_item': old1.id, 'newer_item': '', 'message': 'hello'}]
+        empty_older_item_data = [{'older_item': '', 'newer_item': new1.id, 'message': 'hello'}]
 
-        data = [{
-            'older_item': old1.id, 'newer_item': '', 'message': 'hello'
-        }]
-
-        response = self.post_formset(data, 0)
+        response = self.post_formset(empty_newer_item_data, 0)
 
         self.assertFalse(response.context['formset'][0].is_valid())
 
-        data2 = [{
-            'older_item': '', 'newer_item': new1.id, 'message': 'hello'
-        }]
+        response2 = self.post_formset(empty_older_item_data, 0)
 
-        response = self.post_formset(data2, 0)
-        self.assertFalse(response.context['formset'][0].is_valid())
+        self.assertFalse(response2.context['formset'][0].is_valid())
 
     def test_accept_review_supersedes_approved(self):
         older = MDR.ObjectClass.objects.create(name='2nd', definition='Second')
