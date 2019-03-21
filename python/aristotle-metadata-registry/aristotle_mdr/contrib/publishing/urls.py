b68941a7bb895cd555793302eff8f95bdefc1e56
Sam Spencer <theodore.therone@gmail.com>
2019-03-21 01:51:20 +0000

    improve publishing of metadata items

--- python/aristotle-metadata-registry/aristotle_mdr/contrib/publishing/urls.py
+++ python/aristotle-metadata-registry/aristotle_mdr/contrib/publishing/urls.py
@@ -3,6 +3,6 @@ from aristotle_mdr.contrib.publishing import views
 
 
 urlpatterns = [
-    url(r'^item/(?P<iid>\d+)/publishing/?$', views.VersionPublishMetadataFormView.as_view(), name='item_publish_details'),
+    url(r'^item/(?P<iid>\d+)/publish_versions/?$', views.VersionPublishMetadataFormView.as_view(), name='edit_version_history_visibility'),
     url(r'^publish/(?P<model_name>\w+)/(?P<iid>\d+)?$', views.PublishContentFormView.as_view(), name='publish_item'),
 ]
