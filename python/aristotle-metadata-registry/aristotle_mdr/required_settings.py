d956c08809638abcdf91e355bcf6eed6cdc00c21
Francisco <fragalcer@gmail.com>
2019-03-22 14:31:59 +1100

    Created a Django setting to limit the number of nodes allowed in the general graphical representation. Implemented tests for graphs.

--- python/aristotle-metadata-registry/aristotle_mdr/required_settings.py
+++ python/aristotle-metadata-registry/aristotle_mdr/required_settings.py
@@ -371,3 +371,5 @@ GRAPHQL_ENABLED = False
 
 # Allows migrations to print text on success (disabled during testing)
 MIGRATION_PRINT = True
+
+MAXIMUM_NUMBER_OF_NODES_IN_GENERAL_GRAPHICAL_REPRESENTATION = 200
