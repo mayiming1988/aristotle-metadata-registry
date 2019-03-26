e59e5cef8e9afdb75642cbbf1ebd3c8d952a1e10
Harry White <harry.white424@gmail.com>
2019-03-26 11:25:43 +1100

    Fix form appending and counting

--- assets/src/lib/formset.js
+++ assets/src/lib/formset.js
@@ -20,7 +20,15 @@ export function replacePrefix(element, num_forms) {
 // Adds a row to a formset given a form element and row css selector
 // urlfunc is optional (used for dal init)
 export function addRow(formid, row_selector, urlfunc) {
-    let panelList = $('#' + formid);
+    // Get panel list
+    let panelList = document.getElementById(formid)
+    // If it's a table use the body
+    if (panelList.tagName === 'TABLE') {
+        panelList = panelList.querySelector('tbody')
+    }
+    // Convert to jquery object
+    panelList = $(panelList)
+
     let formstage = $('.formstage#' + formid + ' ' + row_selector)
 
     // Clone the formstage
@@ -41,21 +49,9 @@ export function addRow(formid, row_selector, urlfunc) {
 
     let all_rows = panelList.find(row_selector)
     let num_forms = all_rows.length
-    if (num_forms > 0) {
-        // If there are existing forms insert after last one
-        new_form.insertAfter(all_rows.last());
-    } else {
-        // If this is the first form
-        let add_buttons = panelList.find('.add_code_button')
-        if (add_buttons.length > 0) {
-            // If an add button is in the panel list insert before that
-            let button = add_buttons.first()
-            new_form.insertBefore(button)
-        } else {
-            // otherwise just append to the panel list
-            panelList.append(new_form)
-        }
-    }
+    
+    // Append form to the panel list
+    panelList.append(new_form)
 
     new_form.find(':input').each(function() {
         replacePrefix(this, num_forms)
