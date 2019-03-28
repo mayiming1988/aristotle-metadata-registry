90e0f79c0c742ad1153af2fb41832dd4285019d7
Francisco <fragalcer@gmail.com>
2019-03-28 16:09:05 +1100

    Implemented 'show all Data Elements' for Dataset Specification Clusters.

--- assets/src/pages/datasetSpecification.js
+++ assets/src/pages/datasetSpecification.js
@@ -6,3 +6,19 @@ initItemPage()
 
 // init toggle popovers
 $('[data-toggle="popover"]').popover()
+
+
+// This code is used by the collapsable Data Elements in the "Custers" section of the Dataset Specification page:
+let coll = document.getElementsByClassName("collapsible");
+
+for (let elem of coll) {
+  elem.addEventListener("click", function() {
+    this.classList.toggle("active");
+    let content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
