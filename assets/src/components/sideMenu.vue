3debd97354525cf565e7be3430440ae077dadec4
Harry White <harry.white424@gmail.com>
2019-03-29 16:40:20 +1100

    Created sideMenu component

---
+++ assets/src/components/sideMenu.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="list-group">
+        <a v-for="(item, index) in items" 
+            :key="item" 
+            href="#" 
+            @click="clicked(index)" 
+            :class="[{active: isActive(index)}, 'list-group-item']"
+            >
+            {{ item }}
+        </a>
+    </div>
+</template>
+
+<script>
+/* Github like side menu */
+export default {
+    props: {
+        items: {
+            type: Array,
+            required: true
+        },
+        selected: {
+            type: Number,
+            default: 0
+        }
+    },
+    methods: {
+        clicked: function(i) {
+            this.$emit('selected', i)
+        },
+        isActive: function(i) {
+            return i == this.selected
+        }
+    }
+}
+</script>
