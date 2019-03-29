540f1ac92136d3c89a7140e5d4fecc87ee4f93b7
Harry White <harry.white424@gmail.com>
2019-03-29 16:40:11 +1100

    Added graphs page component

---
+++ assets/src/components/graphs/graphsPage.vue
@@ -0,0 +1,16 @@
+<template>
+    <sideMenu :items="menuItems" />
+</template>
+
+<script>
+import sideMenu from '@/sideMenu.vue'
+
+export default {
+    components: {
+        sideMenu
+    },
+    data: () => ({
+        menuItems: ['General', 'Supersedes', 'Links']
+    })
+}
+</script>
--- assets/src/pages/graphsPage.js
+++ assets/src/pages/graphsPage.js
@@ -2,9 +2,11 @@ import { initCore } from 'src/lib/init.js'
 import renderComponents from 'src/lib/renderComponents.js'
 import 'vis/dist/vis.css'
 import graphicalRepresentation from '@/graphicalRepresentation.vue'
+import graphsPage from '@/graphs/graphsPage.vue'
 
 
 initCore()
 renderComponents({
     'graphical-representation': graphicalRepresentation,
+    'graphs-page': graphsPage
 })
