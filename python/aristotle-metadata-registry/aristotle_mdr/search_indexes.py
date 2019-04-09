8eb239ec9571fbed8b80fab88f4963aa665a08ea
silke <dylan@aristotlemetadata.com>
2019-04-09 14:09:56 +1000

    - Fixed indexing on user name of discussion

--- python/aristotle-metadata-registry/aristotle_mdr/search_indexes.py
+++ python/aristotle-metadata-registry/aristotle_mdr/search_indexes.py
@@ -173,44 +173,49 @@ class ConceptIndex(BaseObjectIndex):
         if record:
             return record.publication_date
 
-# TODO: move to inheritance
-class BaseObjectIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True, use_template=True)
-    modified = indexes.DateTimeField(model_attr='modified')
-    created = indexes.DateTimeField(model_attr='created')
 
-    rendered_search_result = indexes.CharField(indexed=False)
-
-    def prepare_rendered_search_result(self, posting):
-
-        t = loader.get_template(self.template_name)
-        return t.render({'posting': posting})
-
-
-    def get_model(self):
-        raise NotImplementedError  # pragma: no cover -- This should always be overridden
-
-    def index_queryset(self, using=None):
-        """Used when the entire index for model is updated."""
-
-        return self.get_model().objects.filter(modified__lte=timezone.now())
+# # TODO: move to inheritance
+# class BaseObjectIndex(indexes.SearchIndex, indexes.Indexable):
+#     text = indexes.CharField(document=True, use_template=True)
+#     modified = indexes.DateTimeField(model_attr='modified')
+#     created = indexes.DateTimeField(model_attr='created')
+#
+#     rendered_search_result = indexes.CharField(indexed=False)
+#
+#     def prepare_rendered_search_result(self, posting):
+#
+#         t = loader.get_template(self.template_name)
+#         return t.render({'posting': posting})
+#
+#
+#     def get_model(self):
+#         raise NotImplementedError  # pragma: no cover -- This should always be overridden
+#
+#     def index_queryset(self, using=None):
+#         """Used when the entire index for model is updated."""
+#
+#         return self.get_model().objects.filter(modified__lte=timezone.now())
 
 
 class DiscussionIndex(indexes.SearchIndex, indexes.Indexable):
+    """ Index of Discussion posts """
     text = indexes.CharField(document=True, use_template=True)
     title = indexes.CharField(model_attr='title')
+    body = indexes.CharField(model_attr='body')
     modified = indexes.DateTimeField(model_attr='modified')
     created = indexes.DateTimeField(model_attr='created')
-
 
     rendered_search_result = indexes.CharField(indexed=False)
 
     template_name = "search/searchDiscussion.html"
 
-    def prepare_rendered_search_result(self, discussion):
-
+    def prepare_rendered_search_result(self, discussion_post):
+        """
+        Pre-renders all the discussion search results to avoid hitting the database every search
+        """
         t = loader.get_template(self.template_name)
-        return t.render({'discussion': discussion})
+        logger.debug("Post render is: " +  t.render({'discussion_post': discussion_post}))
+        return t.render({'discussion_post': discussion_post})
 
     def get_model(self):
             return models.DiscussionPost
