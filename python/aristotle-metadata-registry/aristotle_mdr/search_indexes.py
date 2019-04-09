efc2eb64bc45c3b9d87d6cee4ca421dd99b067f2
silke <dylan@aristotlemetadata.com>
2019-04-09 13:26:14 +1000

    - Added searchDiscussion.html - Created BaseCommunicationsIndex

--- python/aristotle-metadata-registry/aristotle_mdr/search_indexes.py
+++ python/aristotle-metadata-registry/aristotle_mdr/search_indexes.py
@@ -173,26 +173,44 @@ class ConceptIndex(BaseObjectIndex):
         if record:
             return record.publication_date
 
-
-
-# class BaseCommunicationsIndex(indexes.SearchIndex, indexes.Indexable):
-#     text = indexes.CharField(document=True)
-
-
-class DiscussionIndex(indexes.SearchIndex, indexes.Indexable):
+# TODO: move to inheritance
+class BaseObjectIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     modified = indexes.DateTimeField(model_attr='modified')
     created = indexes.DateTimeField(model_attr='created')
 
     rendered_search_result = indexes.CharField(indexed=False)
 
-
-    template_name = "search/searchDiscussion"
-
-    def prepare_rendered_search_result(self, obj):
+    def prepare_rendered_search_result(self, posting):
 
         t = loader.get_template(self.template_name)
-        return t.render({'object': obj})
+        return t.render({'posting': posting})
+
+
+    def get_model(self):
+        raise NotImplementedError  # pragma: no cover -- This should always be overridden
+
+    def index_queryset(self, using=None):
+        """Used when the entire index for model is updated."""
+
+        return self.get_model().objects.filter(modified__lte=timezone.now())
+
+
+class DiscussionIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    title = indexes.CharField(model_attr='title')
+    modified = indexes.DateTimeField(model_attr='modified')
+    created = indexes.DateTimeField(model_attr='created')
+
+
+    rendered_search_result = indexes.CharField(indexed=False)
+
+    template_name = "search/searchDiscussion.html"
+
+    def prepare_rendered_search_result(self, discussion):
+
+        t = loader.get_template(self.template_name)
+        return t.render({'discussion': discussion})
 
     def get_model(self):
             return models.DiscussionPost
