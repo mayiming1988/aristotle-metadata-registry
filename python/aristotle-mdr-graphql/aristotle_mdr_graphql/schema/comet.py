ccd6443fb4f93daa48bd8792c7b1405eb6156e27
Francisco <fragalcer@gmail.com>
2019-04-16 16:47:39 +1000

    Implemented GraphQL for IndicatorNumeratorDefinition, IndicatorDenominatorDefinition, and DisaggregationDefinition.

--- python/aristotle-mdr-graphql/aristotle_mdr_graphql/schema/comet.py
+++ python/aristotle-mdr-graphql/aristotle_mdr_graphql/schema/comet.py
@@ -1,6 +1,7 @@
 from aristotle_mdr_graphql.fields import AristotleConceptFilterConnectionField
 from comet import models as comet_models
-from aristotle_mdr_graphql.utils import type_from_concept_model
+from aristotle_mdr_graphql.utils import type_from_concept_model, type_from_model, inline_type_from_model
+from graphene_django.filter import DjangoFilterConnectionField
 
 # IndicatorTypeNode = type_from_concept_model(comet_models.IndicatorType)
 IndicatorNode = type_from_concept_model(comet_models.Indicator)
@@ -9,6 +10,9 @@ IndicatorSetNode = type_from_concept_model(comet_models.IndicatorSet)
 # OutcomeAreaNode = type_from_concept_model(comet_models.OutcomeArea)
 QualityStatementNode = type_from_concept_model(comet_models.QualityStatement)
 # FrameworkNode = type_from_concept_model(comet_models.Framework)
+IndicatorNumeratorDefinitionNode = type_from_model(comet_models.IndicatorNumeratorDefinition)
+IndicatorDenominatorDefinitionNode = type_from_model(comet_models.IndicatorDenominatorDefinition)
+IndicatorDisaggregationDefinitionNode = type_from_model(comet_models.IndicatorDisaggregationDefinition)
 
 
 class Query(object):
@@ -20,3 +24,6 @@ class Query(object):
     # outcome_areas = AristotleConceptFilterConnectionField(OutcomeAreaNode)
     quality_statements = AristotleConceptFilterConnectionField(QualityStatementNode)
     # frameworks = AristotleConceptFilterConnectionField(FrameworkNode)
+    indicator_numerator_definitions = DjangoFilterConnectionField(IndicatorNumeratorDefinitionNode)
+    indicator_denominator_definitions = DjangoFilterConnectionField(IndicatorDenominatorDefinitionNode)
+    indicator_disaggregation_definitions = DjangoFilterConnectionField(IndicatorDisaggregationDefinitionNode)
