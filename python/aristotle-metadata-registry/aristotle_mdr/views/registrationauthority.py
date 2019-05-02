fa38924fa6c551727e6bd7badff8549806b06c89
Dylan Silke <d.ylan.s.silke@gmail.com>
2019-05-02 10:07:14 +1000

    Revert "Removed unnecessary code"
    This reverts commit 907a938e

--- python/aristotle-metadata-registry/aristotle_mdr/views/registrationauthority.py
+++ python/aristotle-metadata-registry/aristotle_mdr/views/registrationauthority.py
@@ -378,6 +378,10 @@ class DateFilterView(FilterView, MainPageMixin):
         kwargs = super().get_filterset_kwargs(filterset_class)
         kwargs.update({'registration_authority_id': self.kwargs['iid']})
 
+        if kwargs["data"] is None:
+            # If there were no selections made in the form, set defaults
+            kwargs["data"] = {"status": MDR.STATES.standard,
+                              "registration_date": str(datetime.date.today())}
         return kwargs
 
     def build_downloaders(self, queryset):
