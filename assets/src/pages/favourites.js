9342081fecdd768cc196b6cccb17b47a9ed1d815
Dylan Silke <d.ylan.s.silke@gmail.com>
2019-05-06 10:44:27 +0800

    Return number of item checkboxes on page

--- assets/src/pages/favourites.js
+++ assets/src/pages/favourites.js
@@ -5,17 +5,24 @@ import 'src/styles/aristotle.dashboard.less'
 
 initCore();
 
-let selectAllCheckbox = document.getElementById("select_all_checkbox")
+let selectAllCheckbox = document.getElementById("select_all_checkbox");
 selectAllCheckbox.addEventListener("change", function () {
-        toggle_all_checkboxes(this);
+    toggle_all_checkboxes(this);
+    console.log(item_checkboxes_on_page())
 });
 
 function toggle_all_checkboxes(source) {
     var checkboxes = document.querySelectorAll('input[type="checkbox"]');
     for (var i = 0; i < checkboxes.length; i++) {
-        if (checkboxes[i] != source)
+        if (checkboxes[i] != source) {
             checkboxes[i].checked = source.checked;
+        }
     }
 
+}
+function item_checkboxes_on_page() {
+    var checkboxes = document.getElementsByClassName("checkbox");
+
+    return checkboxes.length
 }
 
