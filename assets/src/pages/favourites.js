00c992f4047fa6b2f6ef025582b28a3cabccf586
Dylan Silke <d.ylan.s.silke@gmail.com>
2019-05-06 10:35:59 +0800

    Select all checkbox now working

--- assets/src/pages/favourites.js
+++ assets/src/pages/favourites.js
@@ -1,6 +1,21 @@
-import { initCore } from 'src/lib/init.js'
+import {initCore} from 'src/lib/init.js'
 
 import 'src/styles/taggle.css'
 import 'src/styles/aristotle.dashboard.less'
 
-initCore()
+initCore();
+
+let selectAllCheckbox = document.getElementById("select_all_checkbox")
+selectAllCheckbox.addEventListener("change", function () {
+        toggle_all_checkboxes(this);
+});
+
+function toggle_all_checkboxes(source) {
+    var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    for (var i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i] != source)
+            checkboxes[i].checked = source.checked;
+    }
+
+}
+
