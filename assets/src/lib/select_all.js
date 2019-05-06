6fb6b37822d3530d121660f6f294a0dee78e5a70
Dylan Silke <d.ylan.s.silke@gmail.com>
2019-05-06 16:48:20 +0800

    Moved to template and lib function

---
+++ assets/src/lib/select_all.js
@@ -0,0 +1,56 @@
+export function initSelectAll() {
+    let selectAllCheckbox = document.getElementById('select-all-checkbox');
+    selectAllCheckbox.addEventListener("change", function () {
+        toggle_all_checkboxes(this);
+        toggle_select_all_div();
+    });
+
+    let selectAllQuerysetButton = document.getElementById("select-all-queryset-button")
+    selectAllQuerysetButton.addEventListener("click", function () {
+        select_all_queryset()
+    });
+}
+
+
+function toggle_all_checkboxes(source) {
+    var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    for (var i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i] != source) {
+            checkboxes[i].checked = source.checked;
+        }
+    }
+}
+
+function toggle_select_all_div() {
+    var select_all = document.getElementById('select-all-div')
+
+    if (select_all.style.display == 'block') {
+        select_all.style.display = 'none';
+    } else {
+        select_all.style.display = 'block';
+    }
+
+}
+
+function select_all_queryset() {
+    let length_queryset = document.getElementById('select-all-queryset-button').getAttribute('data-total-queryset');
+
+    let markup = `
+        All of your ${length_queryset} favourites have been selected 
+        <button class="btn btn-outline btn-outline-info" id="clear-selections-button" type="reset">Clear selections</button>
+        `;
+
+    document.getElementById("select-all-div").innerHTML = markup;
+
+    document.getElementById("clear-selections-button").
+        addEventListener("click", function () {
+            clear_selections();
+    });
+
+    // Select the hidden queryset checkbox
+    document.getElementById("all_in_queryset").checked = true;
+}
+
+function clear_selections() {
+    toggle_select_all_div();
+}
--- python/aristotle-metadata-registry/aristotle_mdr/contrib/favourites/views.py
+++ python/aristotle-metadata-registry/aristotle_mdr/contrib/favourites/views.py
@@ -162,7 +162,7 @@ class FavouritesAndTags(LoginRequiredMixin, ListView):
         context['help'] = self.request.GET.get('help', False)
         context['favourite'] = self.request.GET.get('favourite', False)
         context['tags'] = self.get_tags()
-        context['number_all_favourites'] = self.get_queryset().count()
+        context['count_favourites'] = self.get_queryset().count()
         return context
 
 
