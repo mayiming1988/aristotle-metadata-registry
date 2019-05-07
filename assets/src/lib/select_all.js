478971a4b2feb2a5f27dbc758b353deeac2133f8
Dylan Silke <d.ylan.s.silke@gmail.com>
2019-05-07 09:11:51 +0800

    Fixed repeated usage of select all

--- assets/src/lib/select_all.js
+++ assets/src/lib/select_all.js
@@ -1,19 +1,23 @@
+
 export function initSelectAll() {
+
     let selectAllCheckbox = document.getElementById('select-all-checkbox');
     selectAllCheckbox.addEventListener("change", function () {
         toggle_all_checkboxes(this);
-        toggle_select_all_div();
+        if (document.getElementById('select-all-checkbox').checked) {
+            show_initial_div()
+        }
+        else {
+            // Checkbox is not checked
+            hide_initial_div()
+        }
     });
 
     let selectAllQuerysetButton = document.getElementById("select-all-queryset-button")
     selectAllQuerysetButton.addEventListener("click", function () {
-        select_all_queryset()
+        select_all_queryset();
     });
-
-    var initialText = document.getElementById('select-all-div').innerHTML;
-
 }
-
 
 function toggle_all_checkboxes(source) {
     var checkboxes = document.querySelectorAll('input[type="checkbox"]');
@@ -22,40 +26,55 @@ function toggle_all_checkboxes(source) {
             checkboxes[i].checked = source.checked;
         }
     }
+    document.getElementById("all_in_queryset").checked = false;
 }
 
-function toggle_select_all_div() {
-    var select_all = document.getElementById('select-all-div')
+function show_initial_div() {
+    let initial = document.getElementById('initial-div')
+    initial.style.display = 'block';
+}
 
-    if (select_all.style.display == 'block') {
-        select_all.style.display = 'none';
-    } else {
-        select_all.style.display = 'block';
-    }
+function hide_initial_div() {
+    let initial = document.getElementById('initial-div')
+    initial.style.display = 'none';
 
 }
 
 function select_all_queryset() {
-    let length_queryset = document.getElementById('select-all-queryset-button').getAttribute('data-total-queryset');
+    swap_divs();
 
-    let markup = `
-        All of your ${length_queryset} favourites have been selected 
-        <button class="btn btn-outline btn-outline-info" id="clear-selections-button" type="reset">Clear selections</button>
-        `;
-
-    document.getElementById("select-all-div").innerHTML = markup;
-
-    document.getElementById("clear-selections-button").
-        addEventListener("click", function () {
-            clear_selections();
+    document.getElementById("clear-selections-button").addEventListener("click",
+        function () {
+            clear_selections(this);
     });
-
-    // Select the hidden queryset checkbox
     document.getElementById("all_in_queryset").checked = true;
 }
+function clear_selections(source) {
+    hide_select_all_div()
+    hide_initial_div()
+    toggle_all_checkboxes(source)
+}
 
-function clear_selections() {
-    toggle_select_all_div();
-    document.getElementById("select-all-div").innerHTML = '';
+function swap_divs() {
+    let select_all = document.getElementById('select-all-div')
 
+    if (select_all.style.display == 'block') {
+        // If div is displayed, hide it
+        select_all.style.display = 'none';
+    } else {
+        // Show it
+        select_all.style.display = 'block';
+    }
+    let initial_div = document.getElementById('initial-div')
+
+    if (initial_div.style.display == 'block') {
+        initial_div.style.display = 'none';
+    } else {
+        initial_div.style.display = 'block'
+    }
+}
+
+function hide_select_all_div() {
+    let select_all = document.getElementById('select-all-div')
+    select_all.style.display = 'none';
 }
