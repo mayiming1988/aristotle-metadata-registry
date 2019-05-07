c4d385feb7a9d0b072fb4d22cfa3841b588825a1
Dylan Silke <d.ylan.s.silke@gmail.com>
2019-05-07 13:33:41 +0800

    Refactored tests

--- python/aristotle-metadata-registry/aristotle_mdr/contrib/groups/tests/test_groups.py
+++ python/aristotle-metadata-registry/aristotle_mdr/contrib/groups/tests/test_groups.py
@@ -4,34 +4,24 @@ import aristotle_mdr.tests.utils as utils
 from django.contrib.auth.models import AnonymousUser
 from django.core.cache import cache
 
-import logging
-logger = logging.getLogger(__name__)
-
-
-class GroupsBulkActions(utils.AristotleTestUtils, TestCase):
-    def setUp(self):
-        super().setUp()
-
-        cache.clear()
-
-    def test_anonymous_users_dont_have_any_permission_for_groups(self):
-        self.anonymous_user = AnonymousUser()
-        self.abstract_group = StewardOrganisation.objects.create(name="Test Steward Organisation")
-        for perm in self.abstract_group.role_permissions.keys():
-            self.assertEqual(self.abstract_group.user_has_permission(self.anonymous_user, perm), False)
-
+from django.contrib.auth import get_user_model
+from aristotle_mdr.utils import setup_aristotle_test_environment
 
 from aristotle_mdr.contrib.groups.base import (
     AbstractMembership,
     AbstractGroup
 )
-
 from django.utils.translation import ugettext_lazy as _
 from model_utils import Choices
 
+import logging
+logger = logging.getLogger(__name__)
+
+User = get_user_model()
+setup_aristotle_test_environment()
+
 
 class FakeGroup(AbstractGroup):
-
     roles = Choices(
         ('best', _('Best')),
         ('ok', _('Ok')),
@@ -61,6 +51,17 @@ class FakeGroup(AbstractGroup):
 class FakeMembership(AbstractMembership):
     group_class = FakeGroup
 
+
+class GroupsBulkActions(utils.AristotleTestUtils, TestCase):
+    def setUp(self):
+        super().setUp()
+        cache.clear()
+
+    def test_anonymous_users_dont_have_any_permission_for_groups(self):
+        self.anonymous_user = AnonymousUser()
+        self.abstract_group = StewardOrganisation.objects.create(name="Test Steward Organisation")
+        for perm in self.abstract_group.role_permissions.keys():
+            self.assertEqual(self.abstract_group.user_has_permission(self.anonymous_user, perm), False)
 
 class GroupTestCase(TestCase):
 
