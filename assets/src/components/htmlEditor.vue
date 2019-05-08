3d4aa4cfbf54a517babce91029c697c96772939e
Harry White <harry.white424@gmail.com>
2019-05-08 14:10:19 +0800

    htmlEditor vue component rendering

---
+++ assets/src/components/htmlEditor.vue
@@ -0,0 +1,30 @@
+<template>
+    <textarea ref="te" class="ckeditor-vue" :value="initial" />
+</template>
+
+<script>
+import { initCKEditorFromTextarea } from 'src/lib/ckeditor_simple_init.js'
+
+export default {
+    props: {
+        value: {
+            type: String,
+            default: 'Spicy'
+        },
+        jsonConfig: {
+            type: String,
+            default: '{}'
+        }
+    },
+    data: () => ({
+        initial: '',
+    }),
+    created: function() {
+        this.initial = this.value
+        this.config = JSON.parse(this.jsonConfig)
+    },
+    mounted: function() {
+        initCKEditorFromTextarea(this.$refs.te, this.config)
+    }
+}
+</script>
