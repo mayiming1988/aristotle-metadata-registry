ce8dd1387fd31215db22de46cb09b161fe2f0e5b
Harry White <harry.white424@gmail.com>
2019-05-21 13:29:08 +1000

    Efficiency improvement to concept url slugify

--- python/aristotle-metadata-registry/aristotle_mdr/utils/utils.py
+++ python/aristotle-metadata-registry/aristotle_mdr/utils/utils.py
@@ -91,13 +91,13 @@ def get_download_template_path_for_item(item, template_type, subpath=''):
 
 
 def url_slugify_concept(item):
-    item = item.item
+    item_model = item.item_type.model_class()
     slug = slugify(item.name)[:50]
     if not slug:
         slug = "--"
     return reverse(
         "aristotle:item",
-        kwargs={'iid': item.pk, 'model_slug': item._meta.model_name, 'name_slug': slug}
+        kwargs={'iid': item.pk, 'model_slug': item_model._meta.model_name, 'name_slug': slug}
     )
 
 
