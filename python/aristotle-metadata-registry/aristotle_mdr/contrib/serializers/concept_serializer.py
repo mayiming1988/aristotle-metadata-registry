6c9c7442fe38c3a151869971614bddac23f09cbb
Dylan Silke <d.ylan.s.silke@gmail.com>
2019-06-06 09:40:41 +1000

    Implemented serializer factory, base deserializer

--- python/aristotle-metadata-registry/aristotle_mdr/contrib/serializers/concept_serializer.py
+++ python/aristotle-metadata-registry/aristotle_mdr/contrib/serializers/concept_serializer.py
@@ -2,9 +2,12 @@
 Serializer
 """
 from rest_framework import serializers
-from django.core.serializers.base import Serializer
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
 
-import json
+from django.core.serializers.base import Serializer, DeserializedObject, build_instance
+
+import io
 
 import aristotle_mdr.models as MDR
 
@@ -18,6 +21,7 @@ class ConceptSerializer(serializers.ModelSerializer):
         fields = ('id', 'uuid', 'name', 'version', 'definition', 'short_definition')
 
 
+
 class Serializer(Serializer):
     """This is a django serializer that has a 'composed' DRF Framework inside. """
 
@@ -27,22 +31,53 @@ class Serializer(Serializer):
 
     def serialize(self, queryset, stream=None, fields=None, use_natural_foreign_keys=False,
                   use_natural_primary_keys=False, progress_output=None, **options):
+
         concept = queryset[0]
         concept_serializer = ConceptSerializer(concept)
 
-        self.data = json.dumps(concept_serializer.data)
+        self.data = JSONRenderer().render(concept_serializer.data)
 
     def getvalue(self):
+        # Get value must be overridden because django-reversion calls *getvalue* rather than serialize directly
         return self.data
 
 
+def Deserializer(json, **options):
+    """ Deserialize JSON back into Django ORM instances.
+        Django deserializers yield a DeserializedObject generator.
+        DeserializedObjects are thin wrappers over POPOs. """
 
-class BaseConceptSerializer(serializers.ModelSerializer):
-    """ An 'abstract' concept serializer that has its attributes dynamically set """
+    # Parse a stream into Python's native datatype
+    stream = io.BytesIO(json)
+    data = JSONParser().parse(stream)
 
 
-def model_serializer_factory():
-    """ Generalized serializer factory to dynamically set form fields """
-    pass
+    # Restore those datatypes into a dictionary of validated data
+    serializer = Serializer(data=data)
+    serializer.is_valid()
+
+    # concept =  build_instance()
+
+
+class BaseSerializer(serializers.ModelSerializer):
+    """ An 'abstract' concept serializer that has its attributes dynamically set.
+        Used for simple concepts that may have different fields, but not aristotleComponents """
+
+    class Meta:
+        model = None
+        fields = tuple()
+
+
+class ModelSerializerFactory():
+    """ Generalized serializer factory to dynamically set form fields for simpler concepts """
+
+    def _get_extra_fields(attrs):
+        """Internal helper function to get the extra fields.
+           Returns a dictionary"""
+        pass
+
+    def generate_serializer(self):
+        """ Function that actually generates the serializer """
+        pass
 
 
