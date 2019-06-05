df0854f9913ba16c4fe18c28da5dee2fecf16df5
Harry White <harry.white424@gmail.com>
2019-06-05 10:44:03 +1000

    Refactor item viewed signal

--- python/aristotle-metadata-registry/aristotle_mdr/contrib/view_history/signals.py
+++ python/aristotle-metadata-registry/aristotle_mdr/contrib/view_history/signals.py
@@ -1,40 +1,26 @@
 from datetime import timedelta
 from django.contrib.auth import get_user_model
-from django.dispatch import Signal
 from django.utils.timezone import now
 
 from aristotle_mdr.contrib.async_signals.utils import safe_object
+from aristotle_mdr.contrib.view_history.models import UserViewHistory
 
-import logging
-
-logger = logging.getLogger(__name__)
-logger.debug("Logging started for " + __name__)
-
-
-metadata_item_viewed = Signal(providing_args=["user"])
 User = get_user_model()
 
 
 def item_viewed_action(message):
     instance = safe_object(message)
-    if not instance:
-        return
-    if message['user'] is None:
-        # Don't accept anonymous users.
-        return
-    try:
+
+    # Don't accept anonymous users.
+    if message['user'] is not None:
+
         user = User.objects.get(pk=message['user'])
-    except:
-        # TODO: Maybe log this
-        # If we don't get a valid user, then don't try and assign to them.
-        return
-    recently = now() - timedelta(minutes=30)
-    if user.recently_viewed_metadata.filter(view_date__gt=recently, concept=instance).exists():
-        return
+        recently = now() - timedelta(minutes=30)
 
-    from .models import UserViewHistory
+        # Create history if a recent one doesn't exist
+        if not user.recently_viewed_metadata.filter(view_date__gt=recently, concept=instance).exists():
 
-    UserViewHistory.objects.create(
-        concept=instance,
-        user=user
-    )
+            UserViewHistory.objects.create(
+                concept=instance,
+                user=user
+            )
