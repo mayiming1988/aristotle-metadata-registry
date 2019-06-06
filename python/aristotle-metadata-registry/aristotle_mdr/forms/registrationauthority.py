a02be909cae7d1e033b80f0f74356b54e1db60da
Harry White <harry.white424@gmail.com>
2019-06-07 09:16:46 +1000

    Edit registration form with custom validation

--- python/aristotle-metadata-registry/aristotle_mdr/forms/registrationauthority.py
+++ python/aristotle-metadata-registry/aristotle_mdr/forms/registrationauthority.py
@@ -12,3 +12,28 @@ class CreateRegistrationAuthorityForm(StewardOrganisationRestrictedChoicesForm):
     class Meta:
         model = MDR.RegistrationAuthority
         fields = ['name', 'definition', 'stewardship_organisation']
+
+
+class EditRegistationAuthorityForm(forms.ModelForm):
+    class Meta:
+        model = MDR.RegistrationAuthority
+        fields = [
+            'locked_state',
+            'public_state',
+            'notprogressed',
+            'incomplete',
+            'candidate',
+            'recorded',
+            'qualified',
+            'standard',
+            'preferred',
+            'superseded',
+            'retired',
+        ]
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        # Locked must be lower than public
+        if cleaned_data['locked_state'] >= cleaned_data['public_state']:
+            raise forms.ValidationError('Locked state must be lower than public state')
