6779f8d526dbfb8bab1e75c85b67f7a772714582
Dylan Silke <d.ylan.s.silke@gmail.com>
2019-06-06 15:18:15 +1000

    Implemented custom serialization logic for StewardOrganisation

--- python/aristotle-metadata-registry/aristotle_mdr/contrib/serializers/concept_serializer.py
+++ python/aristotle-metadata-registry/aristotle_mdr/contrib/serializers/concept_serializer.py
@@ -2,15 +2,14 @@
 Serializer
 """
 from rest_framework import serializers
-from rest_framework.parsers import JSONParser
 
 from django.core.serializers.base import Serializer, DeserializedObject, build_instance
 
-from aristotle_mdr.required_settings import WHITELISTED_SUBSERIALIZED_FIELDS
 from aristotle_mdr.contrib.custom_fields.models import CustomValue
 from aristotle_mdr.contrib.slots.models import Slot
+from aristotle_mdr.contrib.identifiers.models import ScopedIdentifier
+from aristotle_mdr.models import RecordRelation
 
-import io
 import json
 
 import logging
@@ -22,17 +21,12 @@ def Deserializer(json, **options):
     """ Deserialize JSON back into Django ORM instances.
         Django deserializers yield a DeserializedObject generator.
         DeserializedObjects are thin wrappers over POPOs. """
+    raise NotImplementedError
 
-    # Parse a stream into Python's native datatype
-    stream = io.BytesIO(json.encode('utf-8'))
-
-    data = JSONParser().parse(stream)
-
-    # Restore those datatypes into a dictionary of validated data
-    serializer = Serializer(data=data)
-    serializer.is_valid()
-
-    # concept =  build_instance()'
+class IdentifierSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ScopedIdentifier
+        fields = ['namespace', 'identifier', 'version', 'order']
 
 
 class CustomValuesSerializer(serializers.ModelSerializer):
@@ -45,6 +39,12 @@ class SlotsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Slot
         fields = ['name', 'length', 'value', 'order', 'permission']
+
+
+class OrganisationRecordsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RecordRelation
+        fields = ['organization_record', 'type']
 
 
 class BaseSerializer(serializers.ModelSerializer):
@@ -75,50 +75,51 @@ class ModelSerializerFactory():
     def _get_relation_fields(self, concept):
         """ Internal helper function to get related fields
             Returns a tuple of fields"""
-        whitelisted_fields = ['workgroup', 'submitter', 'stewardship_organisation',
-                              'statistical_unit_of']
-        related_fields = []
+        whitelisted_fields = ['workgroup', 'submitter']
 
+        related_fields = []
         for field in concept._meta.get_fields():
             if not field.name.startswith('_'):
+                # Don't serialize internal fields
                 if field.is_relation:
                     if hasattr(field, 'get_accessor_name'):
-                        related_fields.append(field.get_accessor_name())
+                        relatd_fields.append(field.get_accessor_name())
                     else:
                         related_fields.append(field.name)
 
         return tuple([field for field in related_fields if field in whitelisted_fields])
 
-    def _get_subserialized_relation_fields(self, fields):
-        """ Internal helper function to get list of fields that will be subserialized
-         Returns a tuple of fields """
-        return tuple([field for field in fields if field in WHITELISTED_SUBSERIALIZED_FIELDS])
-
     def _get_model(self, concept):
         return concept.__class__
 
-    def _generate_subserializer(self, field):
-        pass
-
     def generate_serializer(self, concept):
         """ Generate the serializer class """
+        universal_fields = ('slots', 'customvalue_set', 'org_records', 'identifiers', 'stewardship_organisation')
+
         concept_model = self._get_model(concept)
 
         concept_fields = self._get_concept_fields(concept)
-
         relation_fields = self._get_relation_fields(concept)
+
+        included_fields = concept_fields + relation_fields + universal_fields
 
         class Serializer(BaseSerializer):
             slots = SlotsSerializer(many=True)
             customvalue_set = CustomValuesSerializer(many=True)
+            identifiers = IdentifierSerializer(many=True)
+
+            stewardship_organisation = serializers.PrimaryKeyRelatedField(
+                pk_field=serializers.UUIDField(format='hex'),
+                read_only=True)
 
             class Meta:
                 model = concept_model
-                fields = concept_fields + self.UNIVERSAL_FIELDS
+                fields = included_fields
 
         return Serializer
 
     def generate_deserializer(self, json):
+        """ Generate the deserializer """
         pass
 
 
@@ -133,16 +134,13 @@ class Serializer(Serializer):
         # Generate the serializer
         ModelSerializer = ModelSerializerFactory().generate_serializer(concept)
 
-        # Instanciate the serializer
+        # Instantiate the serializer
         serializer = ModelSerializer(concept)
 
-
-        # Add the app label as a key to the json
-
-        serializer.data.update({
-            'model': '{}.{}'.format(concept._meta.app_label, concept._meta.object_name)})
-
-        self.data = json.dumps(serializer.data)
+        # Add the app label as a key to the json so that the deserializer can be instantiated
+        data = serializer.data
+        data['model'] = '{}.{}'.format(concept._meta.app_label, concept._meta.object_name)
+        self.data = json.dumps(data)
 
     def getvalue(self):
         # Get value must be overridden because django-reversion calls *getvalue* rather than serialize directly
