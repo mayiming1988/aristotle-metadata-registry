a265b621f807a8ddc7135b1701246c1209db0e98
Francisco <fragalcer@gmail.com>
2019-06-10 13:00:05 +1000

    Fixed format. Nothing serious...

--- python/aristotle-metadata-registry/aristotle_mdr/required_settings.py
+++ python/aristotle-metadata-registry/aristotle_mdr/required_settings.py
@@ -61,7 +61,7 @@ CKEDITOR_UPLOAD_PATH = 'uploads/'
 
 
 # Required for admindocs, see: https://code.djangoproject.com/ticket/21386
-SITE_ID=None
+SITE_ID = None
 
 # This gets called because of the DataElementConcept.property attribute.
 # We can resolve this by explicitly adding the parent pointer field, to squash Error E006
