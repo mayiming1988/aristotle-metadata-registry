e16430f99ae8f17a64b9ce9f6570390607a58f44
Harry White <harry.white424@gmail.com>
2019-06-24 17:13:38 +1000

    Added extra sub serializers

--- python/aristotle-metadata-registry/aristotle_mdr/contrib/serializers/dse_serializers.py
+++ python/aristotle-metadata-registry/aristotle_mdr/contrib/serializers/dse_serializers.py
@@ -1,22 +1,30 @@
 from rest_framework import serializers
-from aristotle_dse.models import DSSClusterInclusion, DSSDEInclusion, DSSGrouping
+from aristotle_mdr.contrib.serializers.concept_serializer import SubSerializer
+from aristotle_dse.models import DSSClusterInclusion, DSSDEInclusion, DSSGrouping, DistributionDataElementPath
 
 excluded_fields = ('dss',)
 
 
-class DSSClusterInclusionSerializer(serializers.ModelSerializer):
+class DSSClusterInclusionSerializer(SubSerializer):
     class Meta:
         model = DSSClusterInclusion
         exclude = excluded_fields
 
 
-class DSSDEInclusionSerializer(serializers.ModelSerializer):
+class DSSDEInclusionSerializer(SubSerializer):
     class Meta:
         model = DSSDEInclusion
         exclude = excluded_fields
 
 
-class DSSGroupingSerializer(serializers.ModelSerializer):
+class DSSGroupingSerializer(SubSerializer):
     class Meta:
         model = DSSGrouping
         exclude = excluded_fields
+
+
+class DistributionDataElementPathSerializer(SubSerializer):
+
+    class Meta:
+        model = DistributionDataElementPath
+        exclude = ('distribution',)
