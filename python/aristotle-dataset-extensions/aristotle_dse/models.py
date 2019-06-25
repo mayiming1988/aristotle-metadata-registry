ff5c0c38d1a7db67766b363d6c419c89473d056a
Francisco <fragalcer@gmail.com>
2019-06-25 14:00:00 +1000

    Finished this implementation.

--- python/aristotle-dataset-extensions/aristotle_dse/models.py
+++ python/aristotle-dataset-extensions/aristotle_dse/models.py
@@ -376,6 +376,7 @@ class DSSInclusion(aristotle.models.aristotleComponent):
         help_text=_("The maximum number of times a item can be included in a dataset")
         )
     cardinality = models.CharField(
+        "Inclusion",
         choices=CARDINALITY,
         default=CARDINALITY.conditional,
         max_length=20,
