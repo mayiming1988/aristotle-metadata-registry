9e77facca9cbcece0232b684e5229923f6f9d766
Dylan Silke <d.ylan.s.silke@gmail.com>
2019-07-05 14:02:35 +1000

    Added test for the differing registration components reporting tool

--- python/aristotle-metadata-registry/aristotle_mdr/tests/main/test_tools.py
+++ python/aristotle-metadata-registry/aristotle_mdr/tests/main/test_tools.py
@@ -3,10 +3,10 @@ from django.utils import timezone
 from django.test import TestCase
 from django.urls import reverse
 
-from django.shortcuts import redirect
 from aristotle_mdr.tests.utils import AristotleTestUtils
-from aristotle_mdr import models
+from aristotle_mdr import models as MDR
 
+import datetime
 
 class AristotleReportingToolsPage(AristotleTestUtils, TestCase):
     url_name = 'reportingTool'
@@ -16,71 +16,71 @@ class AristotleReportingToolsPage(AristotleTestUtils, TestCase):
 
         self.login_superuser()
 
-        self.property_non_std = models.Property.objects.create(
+        self.property_non_std = MDR.Property.objects.create(
             name="My PROPERTY NON STANDARD",
             definition="My prop",
             workgroup=self.wg1,
         )
 
-        self.status_prop_non_standard = models.Status.objects.create(
+        self.status_prop_non_standard = MDR.Status.objects.create(
             concept=self.property_non_std,
             registrationAuthority=self.ra,
             registrationDate=timezone.now(),
-            state=models.STATES.qualified,
+            state=MDR.STATES.qualified,
         )
 
-        self.object_class_non_std = models.Property.objects.create(
+        self.object_class_non_std = MDR.Property.objects.create(
             name="My OBJECT CLASS NON STANDARD",
             definition="My oc...",
             workgroup=self.wg1,
         )
 
-        models.Status.objects.create(
+        MDR.Status.objects.create(
             concept=self.object_class_non_std,
             registrationAuthority=self.ra,
             registrationDate=timezone.now(),
-            state=models.STATES.qualified,
+            state=MDR.STATES.qualified,
         )
 
-        self.dec_non_std = models.DataElementConcept.objects.create(
+        self.dec_non_std = MDR.DataElementConcept.objects.create(
             name="My DEC",
             definition="MY dec...",
             workgroup=self.wg1,
             property=self.property_non_std
         )
 
-        models.Status.objects.create(
+        MDR.Status.objects.create(
             concept=self.property_non_std,
             registrationAuthority=self.ra,
             registrationDate=timezone.now(),
-            state=models.STATES.qualified,
+            state=MDR.STATES.qualified,
         )
 
-        self.vd_non_std = models.ValueDomain.objects.create(
+        self.vd_non_std = MDR.ValueDomain.objects.create(
             name="My Value Domain",
             definition="My vd...",
             workgroup=self.wg1,
         )
 
-        models.Status.objects.create(
+        MDR.Status.objects.create(
             concept=self.vd_non_std,
             registrationAuthority=self.ra,
             registrationDate=timezone.now(),
-            state=models.STATES.qualified,
+            state=MDR.STATES.qualified,
         )
 
-        self.de_std = models.DataElement.objects.create(
+        self.de_std = MDR.DataElement.objects.create(
             name="My Data Element",
             definition="My data elem...",
             workgroup=self.wg1,
             valueDomain=self.vd_non_std,
         )
 
-        models.Status.objects.create(
+        MDR.Status.objects.create(
             concept=self.de_std,
             registrationAuthority=self.ra,
             registrationDate=timezone.now(),
-            state=models.STATES.standard,
+            state=MDR.STATES.standard,
         )
 
         self.logout()
@@ -100,17 +100,17 @@ class AristotleReportingToolsPage(AristotleTestUtils, TestCase):
 
         incomplete_status = '1'
 
-        my_data_element = models.DataElement.objects.create(
+        my_data_element = MDR.DataElement.objects.create(
             name="My lonely data element",
             definition="I am so lonely. I have no subcomponents.",
             workgroup=self.wg1,
         )
 
-        my_status_for_lonely_data_element = models.Status.objects.create(
+        my_status_for_lonely_data_element = MDR.Status.objects.create(
             concept=my_data_element,
             registrationAuthority=self.ra,
             registrationDate=timezone.now(),
-            state=models.STATES.incomplete,
+            state=MDR.STATES.incomplete,
         )
 
         response = self.client.get(
@@ -124,13 +124,13 @@ class AristotleReportingToolsPage(AristotleTestUtils, TestCase):
 
         qualified_status = '4'
 
-        data_element_concept_for_de_qualified = models.DataElementConcept.objects.create(
+        data_element_concept_for_de_qualified = MDR.DataElementConcept.objects.create(
             name="MY DEC for DE Qualified.",
             definition="mY DEC",
             workgroup=self.wg1,
         )
 
-        de_qualified = models.DataElement.objects.create(
+        de_qualified = MDR.DataElement.objects.create(
             name="My Qualified Data Element",
             definition="My non standard data elem...",
             workgroup=self.wg1,
@@ -138,19 +138,19 @@ class AristotleReportingToolsPage(AristotleTestUtils, TestCase):
         )
 
         # Same Status!
-        models.Status.objects.create(
+        MDR.Status.objects.create(
             concept=de_qualified,
             registrationAuthority=self.ra,
             registrationDate=timezone.now(),
-            state=models.STATES.qualified,
+            state=MDR.STATES.qualified,
         )
 
         # Same Status!
-        models.Status.objects.create(
+        MDR.Status.objects.create(
             concept=data_element_concept_for_de_qualified,
             registrationAuthority=self.ra,
             registrationDate=timezone.now(),
-            state=models.STATES.qualified,
+            state=MDR.STATES.qualified,
         )
 
         response = self.client.get(
@@ -164,35 +164,63 @@ class AristotleReportingToolsPage(AristotleTestUtils, TestCase):
 
         prefered_status = '6'
 
-        another_ra = models.RegistrationAuthority.objects.create(
-            name="Me second RA",
+        another_ra = MDR.RegistrationAuthority.objects.create(
+            name="My second RA",
             definition="My second RA",
             stewardship_organisation=self.steward_org_1,
         )
 
-        vd_for_data_element_with_second_ra = models.ValueDomain.objects.create(
+        vd_for_data_element_with_second_ra = MDR.ValueDomain.objects.create(
             name="VD for data element with second ra",
             definition="My VD",
             workgroup=self.wg1,
         )
 
-        data_element_with_second_ra = models.DataElement.objects.create(
+        data_element_with_second_ra = MDR.DataElement.objects.create(
             name="Data Element with another RA",
             definition="Data Element with another RA",
             workgroup=self.wg1,
             valueDomain=vd_for_data_element_with_second_ra
         )
 
-        models.Status.objects.create(
+        MDR.Status.objects.create(
             concept=data_element_with_second_ra,
             registrationAuthority=another_ra,
             registrationDate=timezone.now(),
-            state=models.STATES.preferred,
+            state=MDR.STATES.preferred,
         )
 
         response = self.client.get(
             reverse(
                 'aristotle:reportingTool') + '?ra=' + str(another_ra.id) + '&status=' + prefered_status
         )
-
         self.assertEqual(response.context['object_list'].count(), 1)
+
+    def test_correct_status_for_component_displayed_in_template(self):
+        """Test that the correct status is displayed for components in the template"""
+        # Create a Value Domain
+        value_domain = MDR.ValueDomain.objects.create(
+            name="Value Domain",
+            definition="Value Domain",
+            workgroup=self.wg1,
+        )
+
+        # Create a Data Element Concept
+        data_element = MDR.DataElement.objects.create(
+            name=" Data Element",
+            definition="A data element",
+            workgroup=self.wg1,
+            valueDomain=value_domain,
+        )
+        # Register the Value Domain
+        MDR.Status.objects.create(
+            concept=value_domain,
+            registrationAuthority=self.ra,
+            registrationDate=datetime.datetime("")
+        )
+        # Register the Property as Standard
+
+        # Retire it later
+
+        # Assert that Retired is displayed
+
