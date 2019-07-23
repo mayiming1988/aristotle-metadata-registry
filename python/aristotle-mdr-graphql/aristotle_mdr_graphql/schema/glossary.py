88cf7120f898b3735f2c6194e816393b2d5f1d90
Harry White <harry.white424@gmail.com>
2019-07-23 13:00:06 +1000

    GlossaryItem should be a concept node

--- python/aristotle-mdr-graphql/aristotle_mdr_graphql/schema/glossary.py
+++ python/aristotle-mdr-graphql/aristotle_mdr_graphql/schema/glossary.py
@@ -1,8 +1,8 @@
-from aristotle_mdr_graphql.utils import type_from_model
+from aristotle_mdr_graphql.utils import type_from_concept_model
 from aristotle_mdr_graphql.fields import AristotleFilterConnectionField
 from aristotle_glossary.models import GlossaryItem
 
-GlossaryItemNode = type_from_model(GlossaryItem)
+GlossaryItemNode = type_from_concept_model(GlossaryItem)
 
 
 class Query:
