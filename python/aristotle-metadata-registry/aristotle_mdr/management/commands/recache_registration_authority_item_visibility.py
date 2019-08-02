983ca962e35dd3f5bb33f49ee2b381291be6432b
Dylan Silke <d.ylan.s.silke@gmail.com>
2019-08-02 14:49:43 +1000

    Improved stdout message

--- python/aristotle-metadata-registry/aristotle_mdr/management/commands/recache_registration_authority_item_visibility.py
+++ python/aristotle-metadata-registry/aristotle_mdr/management/commands/recache_registration_authority_item_visibility.py
@@ -34,7 +34,7 @@ class Command(BaseCommand):
             if 'no_index' not in options:
                 # If no-index is not selected as a flag, update the search index
                 connections['default'].get_unified_index().get_index(item.item.__class__).update_object(item.item)
-                self.stdout.write('Update search index for item: {}!'.format(item.id))
+                self.stdout.write('Updated! search index for item: {}!'.format(item.id))
 
             self.stdout.write(' Updated! item (id:%s)' % (item.id))
 
