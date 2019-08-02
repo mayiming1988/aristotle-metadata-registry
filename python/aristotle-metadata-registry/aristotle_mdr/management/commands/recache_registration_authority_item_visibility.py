27ee7e949421cadf65ea3eb48c63bd7f1d9cd6c2
Dylan Silke <d.ylan.s.silke@gmail.com>
2019-08-02 14:39:28 +1000

    Cleaned code

--- python/aristotle-metadata-registry/aristotle_mdr/management/commands/recache_registration_authority_item_visibility.py
+++ python/aristotle-metadata-registry/aristotle_mdr/management/commands/recache_registration_authority_item_visibility.py
@@ -1,10 +1,11 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from aristotle_mdr.models import RegistrationAuthority, _concept
 
 
 class Command(BaseCommand):
     args = '<workgroup_id workgroup_id ...>'
-    help = 'Recomputes and caches the public and locked statuses for the given workgroup(s). This is useful if the registration authorities associated with a workgroup change.'
+    help = 'Recomputes and caches the public and locked statuses for the given workgroup(s). ' \
+           'This is useful if the registration authorities associated with a workgroup change.'
 
     def add_arguments(self, parser):
         super(Command, self).add_arguments(parser)
@@ -16,7 +17,8 @@ class Command(BaseCommand):
         parser.add_argument(
             '--no-index',
             action='store_true',
-            help="If --no-index is selected, this command will not update the search index"
+            help="If --no-index is selected, recache_registration_authority_item_visibility"
+                 " will not update the search index"
         )
 
     def handle(self, *args, **options):
@@ -32,7 +34,7 @@ class Command(BaseCommand):
             if not options['no-index']:
                 # If no-index is not selected as a flag, update the search index
                 connections['default'].get_unified_index().get_index(item.item.__class__).update_object(item.item)
-                
+
             self.stdout.write(' Updated! item (id:%s)' % (item.id))
 
         self.stdout.write('Successfully updated items!')
