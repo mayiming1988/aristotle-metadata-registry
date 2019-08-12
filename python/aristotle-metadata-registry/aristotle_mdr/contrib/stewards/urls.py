085cc68032682d8e6e533670bb6ea1d189320986
Sam Spencer <theodore.therone@gmail.com>
2019-08-12 01:13:58 +0000

    removed dead comment

--- python/aristotle-metadata-registry/aristotle_mdr/contrib/stewards/urls.py
+++ python/aristotle-metadata-registry/aristotle_mdr/contrib/stewards/urls.py
@@ -8,14 +8,4 @@ urlpatterns = [
             namespace="group"
         ),
     ),
-    # url(
-    #     # CHECK - Can I do this? Instead of the commented out code?
-    #     r'', manager.group_backend_factory().get_urls,
-    # ),
-    # url(
-    #     r'', include(
-    #         manager.group_backend_factory().get_urls(),
-    #         namespace="group"
-    #     ),
-    # ),
 ]
