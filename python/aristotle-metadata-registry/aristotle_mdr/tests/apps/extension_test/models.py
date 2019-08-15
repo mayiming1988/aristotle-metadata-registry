74e0957b0c13fa1f368229d7e664302a88cfba54
Francisco Gallardo <fragalcer@gmail.com>
2019-08-15 12:33:19 +1000

    Trying to fix errors.

--- python/aristotle-metadata-registry/aristotle_mdr/tests/apps/extension_test/models.py
+++ python/aristotle-metadata-registry/aristotle_mdr/tests/apps/extension_test/models.py
@@ -23,9 +23,9 @@ class Questionnaire(aristotle_mdr.models.concept):
     # Questionnaire is a test of a lazy developer who has done the bare minimum
     # To get an object in the system. This is a test of how little a dev can to
     # get a functional object. Ideally the string 'Questionnaire' should exist only here.
-    edit_page_excludes = ['questions', 'respondent_classes','targetrespondentclass']
+    edit_page_excludes = ['questions', 'respondent_classes', 'targetrespondentclass']
     through_edit_excludes = ['respondent_classes']
-    admin_page_excludes = ['respondent_classes','targetrespondentclass']
+    admin_page_excludes = ['respondent_classes', 'targetrespondentclass']
     # template = "extension_test/concepts/question.html"  # Blank to test default template
     questions = models.ManyToManyField(
         Question,
