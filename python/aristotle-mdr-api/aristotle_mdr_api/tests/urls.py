3d19435710941a8f3b8df6dcb3dcdbfcc6218c21
Dylan Silke <d.ylan.s.silke@gmail.com>
2019-08-15 13:27:45 +1000

    Fixed all pre 2.1 Django URLs

--- python/aristotle-mdr-api/aristotle_mdr_api/tests/urls.py
+++ python/aristotle-mdr-api/aristotle_mdr_api/tests/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import include, url
 urlpatterns = [
     url(r'^', include('aristotle_mdr.urls')),
     url(r'^browse/', include('aristotle_mdr.contrib.browse.urls')),
-    url(r'^help/', include('aristotle_mdr.contrib.help.urls', app_name="aristotle_help", namespace="aristotle_help")),
+    url(r'^help/', include(('aristotle_mdr.contrib.help.urls', "aristotle_help"), namespace="aristotle_help")),
     url(r'^api/', include('aristotle_mdr_api.urls'))
 ]
