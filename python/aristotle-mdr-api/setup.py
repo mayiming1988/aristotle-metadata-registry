db679814a0b0e2d245825c91451b5a01b567c961
Francisco Gallardo <fragalcer@gmail.com>
2019-08-15 11:28:19 +1000

    Updated pipfile.

--- python/aristotle-mdr-api/setup.py
+++ python/aristotle-mdr-api/setup.py
@@ -31,7 +31,7 @@ setup(
     ],
     install_requires=[
         'djangorestframework>=3.8',
-        'django-filter~=2.2.0',
+        'django-filter',
         'django-jsonforms',
         'coreapi',
         'drf-yasg==1.16.0',  # Pinning it because it tries to import packaging
