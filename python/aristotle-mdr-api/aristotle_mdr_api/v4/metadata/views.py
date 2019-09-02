154b33d3688a8b213ba02fe5098a0331a966fd04
Francisco Gallardo <fragalcer@gmail.com>
2019-09-02 14:49:04 +1000

    Implemented basic functionality.

--- python/aristotle-mdr-api/aristotle_mdr_api/v4/metadata/views.py
+++ python/aristotle-mdr-api/aristotle_mdr_api/v4/metadata/views.py
@@ -1,15 +1,15 @@
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.reverse import reverse
-from rest_framework.viewsets import ModelViewSet
 from django.template.defaultfilters import slugify
 from rest_framework.response import Response
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
-from aristotle_mdr_api.v4.permissions import AuthCanViewEdit, UnAuthenticatedUserCanView
-from aristotle_mdr_api.v4.concepts import serializers
+from aristotle_mdr_api.v4.permissions import UnAuthenticatedUserCanView
 from aristotle_mdr.contrib.serializers.concept_serializer import ConceptSerializerFactory
 from aristotle_mdr.models import _concept
+from aristotle_mdr.utils import get_concept_models
+
 
 import logging
 logger = logging.getLogger(__name__)
@@ -31,39 +31,38 @@ class GetMetadataTypeFromUUID(APIView):
         item = get_object_or_404(_concept, uuid=item_uuid)
 
         return HttpResponseRedirect(
-            redirect_to=reverse("api_v4:metadata:generic_metadata_serialiser_api_endpoint",
-                                kwargs={"metadata_type": slugify(item.item_type),
-                                        "item_uuid": item_uuid})
+            redirect_to=reverse(
+                "api_v4:metadata:generic_metadata_serialiser_api_endpoint",
+                kwargs={
+                    "metadata_type": slugify(item.item_type.model),
+                    "item_uuid": item_uuid,
+                }
+            )
         )
 
 
-class GenericMetadataSerialiserAPIView(generics.ListCreateAPIView):  # CHANGE THIS ONE TO RETRIEVEUPDATEAPIVIEW.
+class GenericMetadataSerialiserAPIView(generics.RetrieveUpdateAPIView):
+
+    lookup_field = 'uuid'
+    lookup_url_kwarg = 'item_uuid'
 
     permission_classes = (UnAuthenticatedUserCanView,)
 
     def dispatch(self, request, *args, **kwargs):
+
         self.metadata_type = kwargs.get("metadata_type")
-        self.item_uuid = kwargs.get("item_uuid")
-        # item = get_object_or_404(type(self.item), uuid=self.item_uuid)
-        # self.item = get_object_or_404(self.item.item_type.model_class(), uuid=self.item_uuid)
 
-        print("THIS IS THE ITEM")
-        print(self.get_object())
-        # print("THIS IS THE CONTENT TYPE:")
-        # print()
-        # print("THIS IS THE OBJECT:")
-        # new = get_object_or_404(type(self.item), uuid=self.item_uuid)
-        # print(new)
-        # print(type(new))
-
+        for model in get_concept_models():
+            if slugify(model.__name__) == self.metadata_type:
+                self.model_instance = model
         return super().dispatch(request, *args, **kwargs)
 
-    def get_queryset(self):  # OVERRIDE GET QUERYSET!!!!
-        return self.item.item_type.model_class().objects.get(uuid=self.item_uuid)
+    def get_queryset(self):
+        return self.model_instance.objects.all()
 
     def get_serializer(self, *args, **kwargs):
-        serialiser_model = ConceptSerializerFactory().generate_serializer(self.item)
-        serialiser = serialiser_model(self.item)
+        serialiser_model = ConceptSerializerFactory().generate_serializer(self.get_object())
+        serialiser = serialiser_model(self.get_object())
         return serialiser
 
     def update(self, request, *args, **kwargs):
