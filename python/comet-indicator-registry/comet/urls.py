26535b595971dd59c386df38b323664ae49310f9
Francisco Gallardo <fragalcer@gmail.com>
2019-09-12 17:06:46 +1000

    Improved paths.

--- python/comet-indicator-registry/comet/urls.py
+++ python/comet-indicator-registry/comet/urls.py
@@ -1,15 +1,13 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from comet import views
 from django.views.generic import TemplateView
-from aristotle_mdr.contrib.generic.views import GenericAlterManyToManyView
-from comet import models
 
 
 urlpatterns = [
-    url(r'^/?$', TemplateView.as_view(template_name='comet/static/about_comet_mdr.html')),
+    re_path(r'^/?$', TemplateView.as_view(template_name='comet/static/about_comet_mdr.html')),
 
     # These are required for about pages to work. Include them, or custom items will die!
-    url(r'^about/(?P<template>.+)/?$', views.DynamicTemplateView.as_view(), name="about"),
-    url(r'^about/?$', TemplateView.as_view(template_name='comet/static/about_comet_mdr.html'), name="about"),
+    re_path(r'^about/(?P<template>.+)/?$', views.DynamicTemplateView.as_view(), name="about"),
+    re_path(r'^about/?$', TemplateView.as_view(template_name='comet/static/about_comet_mdr.html'), name="about"),
 ]
