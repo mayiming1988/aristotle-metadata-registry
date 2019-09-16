b600e0ce7d05d7bbd909fc056a41894bc673df6c
Francisco Gallardo <fragalcer@gmail.com>
2019-09-16 15:04:42 +1000

    Several improvements.

--- python/aristotle-mdr-api/aristotle_mdr_api/v4/metadata/urls.py
+++ python/aristotle-mdr-api/aristotle_mdr_api/v4/metadata/urls.py
@@ -37,5 +37,5 @@ def create_metadata_urls_dynamically():
 
 urlpatterns = [
     path('<uuid:item_uuid>/', views.GetMetadataTypeFromUuidAndRedirect.as_view(), name='get_metadata_type_from_uuid'),
-    path('<str:metadata_type>/<uuid:item_uuid>/', views.GenericMetadataSerialiserAPIView.as_view(), name='generic_metadata_serialiser_api_endpoint'),
+    path('<str:metadata_type>/uuid/<uuid:item_uuid>/', views.GenericMetadataSerialiserAPIView.as_view(), name='generic_metadata_serialiser_api_endpoint'),
 ] + create_metadata_urls_dynamically()
