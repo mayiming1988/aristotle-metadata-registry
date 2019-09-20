870770e1e97a9fee2c640f3ddafa1bdcce5a7473
Francisco Gallardo <fragalcer@gmail.com>
2019-09-20 15:42:03 +1000

    Removed unnecessary item clone function and tests.

--- python/aristotle-metadata-registry/aristotle_mdr/views/editors.py
+++ python/aristotle-metadata-registry/aristotle_mdr/views/editors.py
@@ -1,9 +1,9 @@
+import reversion
+import uuid
 from django.http import HttpResponseRedirect
 from django.views.generic import UpdateView, FormView
 from django.views.generic.detail import SingleObjectMixin
 from django.db import transaction
-
-import reversion
 from reversion.models import Version
 
 from aristotle_mdr.utils import (
@@ -338,27 +338,6 @@ class CloneItemView(ExtraFormsetMixin, ConceptEditFormView, SingleObjectMixin, F
                 item.save()
 
             return HttpResponseRedirect(url_slugify_concept(item))
-
-    def clone_components(self, clone):
-        original = self.item
-        fields = getattr(self.model, 'clone_fields', [])
-        for field_name in fields:
-            field = self.model._meta.get_field(field_name)
-            remote_field_name = field.remote_field.name
-            manager = getattr(original, field.get_accessor_name(), None)
-            if manager is None:
-                components = []
-            else:
-                components = manager.all()
-
-            new_components = []
-            for component in components:
-                # Set pk to none so we insert instead of update
-                component.pk = None
-                # Set the remote field to the clone
-                setattr(component, remote_field_name, clone)
-                new_components.append(component)
-            field.related_model.objects.bulk_create(new_components)
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
