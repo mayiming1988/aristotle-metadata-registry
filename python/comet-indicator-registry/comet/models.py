85e14f2636900b115b773496101bfc78f421360f
Francisco Gallardo <fragalcer@gmail.com>
2019-10-01 10:37:28 +1000

    Improved comet models file styling.

--- python/comet-indicator-registry/comet/models.py
+++ python/comet-indicator-registry/comet/models.py
@@ -1,21 +1,74 @@
 from __future__ import unicode_literals
-
 from typing import List
+from mptt.models import MPTTModel, TreeForeignKey
+from model_utils.models import TimeStampedModel
 
 from django.db import models
 from django.utils.translation import ugettext as _
-
-from mptt.models import MPTTModel, TreeForeignKey
-from model_utils.models import TimeStampedModel
 
 import aristotle_mdr.models as MDR
 import aristotle_dse.models as aristotle_dse
 from aristotle_mdr.fields import ConceptForeignKey, ConceptManyToManyField
 from aristotle_mdr.utils.model_utils import (
-    ManagedItem,
     aristotleComponent,
 )
 from comet.managers import FrameworkDimensionManager
+
+
+class Framework(MDR.concept):
+    template = "comet/framework.html"
+    # parentFramework = ConceptForeignKey('Framework', blank=True, null=True, related_name="childFrameworks")
+
+    serialize_weak_entities = [
+        ('dimensions', 'frameworkdimension_set'),
+    ]
+
+    def root_dimensions(self):
+        return self.frameworkdimension_set.all().filter(
+            parent=None
+        )
+
+
+class FrameworkDimension(MPTTModel, TimeStampedModel, aristotleComponent):
+    parent_field_name = 'framework'
+
+    objects = FrameworkDimensionManager()
+    framework = ConceptForeignKey(Framework, on_delete=models.CASCADE)
+    name = models.CharField(max_length=2048)
+    description = MDR.RichTextField(blank=True)
+    parent = TreeForeignKey(
+        'self',
+        null=True,
+        blank=True,
+        to_field='uuid',
+        on_delete=models.CASCADE,
+    )
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
+
+    @property
+    def parentItem(self):
+        return self.framework
+
+    def __str__(self):
+        return self.name
+
+
+class OutcomeArea(MDR.concept):
+    template = "comet/outcomearea.html"
+
+
+class QualityStatement(MDR.concept):
+    template = "comet/qualitystatement.html"
+
+    institutional_environment = MDR.RichTextField(blank=True)
+    timeliness = MDR.RichTextField(blank=True)
+    accessibility = MDR.RichTextField(blank=True)
+    interpretability = MDR.RichTextField(blank=True)
+    relevance = MDR.RichTextField(blank=True)
+    accuracy = MDR.RichTextField(blank=True)
+    coherence = MDR.RichTextField(blank=True)
 
 
 class Indicator(MDR.concept):
@@ -24,10 +77,10 @@ class Indicator(MDR.concept):
     and that provides relevant and actionable information about population or system performance.
     """
     # Subclassing from DataElement causes indicators to present as DataElements, which isn't quite right.
+    template = "comet/indicator.html"
     backwards_compatible_fields = ['representation_class']
 
-    template = "comet/indicator.html"
-    outcome_areas = ConceptManyToManyField('OutcomeArea', related_name="indicators", blank=True)
+    outcome_areas = ConceptManyToManyField(OutcomeArea, related_name="indicators", blank=True)
 
     computation_description = MDR.RichTextField(blank=True)
     computation = MDR.RichTextField(blank=True)
@@ -37,14 +90,14 @@ class Indicator(MDR.concept):
     disaggregation_description = MDR.RichTextField(blank=True)
 
     quality_statement = ConceptForeignKey(
-        "QualityStatement",
+        QualityStatement,
         null=True, blank=True, on_delete=models.SET_NULL,
         help_text=_("A statement of multiple quality dimensions for the purpose of assessing the quality of the data for reporting against this Indicator.")
     )
     rationale = MDR.RichTextField(blank=True)
     benchmark = MDR.RichTextField(blank=True)
     reporting_information = MDR.RichTextField(blank=True)
-    dimensions = ConceptManyToManyField("FrameworkDimension", related_name="indicators", blank=True)
+    dimensions = ConceptManyToManyField(FrameworkDimension, related_name="indicators", blank=True)
 
     serialize_weak_entities = [
         ('numerators', 'indicatornumeratordefinition_set'),
@@ -52,6 +105,33 @@ class Indicator(MDR.concept):
         ('disaggregators', 'indicatordisaggregationdefinition_set'),
     ]
     clone_fields = ['indicatornumeratordefinition', 'indicatordenominatordefinition', 'indicatordisaggregationdefinition']
+
+    @property
+    def numerators(self):
+        return MDR.DataElement.objects.filter(
+            indicatornumeratordefinition__indicator=self
+        )
+
+    @property
+    def denominators(self):
+        return MDR.DataElement.objects.filter(
+            indicatordenominatordefinition__indicator=self
+        )
+
+    @property
+    def disaggregators(self):
+        return MDR.DataElement.objects.filter(
+            indicatordisaggregationdefinition__indicator=self
+        )
+
+    def add_numerator(self, **kwargs):
+        self.add_component(model_class=IndicatorNumeratorDefinition, **kwargs)
+
+    def add_denominator(self, **kwargs):
+        self.add_component(model_class=IndicatorDenominatorDefinition, **kwargs)
+
+    def add_disaggregator(self, **kwargs):
+        self.add_component(model_class=IndicatorDisaggregationDefinition, **kwargs)
 
     def add_component(self, model_class, **kwargs):
         kwargs.pop('indicator', None)
@@ -65,33 +145,6 @@ class Indicator(MDR.concept):
         else:
             order = max_order[0] + 1
         return model_class.objects.create(indicator=self, order=order, **kwargs)
-
-    @property
-    def numerators(self):
-        return MDR.DataElement.objects.filter(
-            indicatornumeratordefinition__indicator=self
-        )
-
-    def add_numerator(self, **kwargs):
-        self.add_component(model_class=IndicatorNumeratorDefinition, **kwargs)
-
-    @property
-    def denominators(self):
-        return MDR.DataElement.objects.filter(
-            indicatordenominatordefinition__indicator=self
-        )
-
-    def add_denominator(self, **kwargs):
-        self.add_component(model_class=IndicatorDenominatorDefinition, **kwargs)
-
-    @property
-    def disaggregators(self):
-        return MDR.DataElement.objects.filter(
-            indicatordisaggregationdefinition__indicator=self
-        )
-
-    def add_disaggregator(self, **kwargs):
-        self.add_component(model_class=IndicatorDisaggregationDefinition, **kwargs)
 
 
 class IndicatorDataElementBase(aristotleComponent):
@@ -163,59 +216,3 @@ class IndicatorInclusion(aristotleComponent):
 
     class Meta:
         ordering = ['order']
-
-
-class OutcomeArea(MDR.concept):
-    template = "comet/outcomearea.html"
-
-
-class QualityStatement(MDR.concept):
-    template = "comet/qualitystatement.html"
-
-    institutional_environment = MDR.RichTextField(blank=True)
-    timeliness = MDR.RichTextField(blank=True)
-    accessibility = MDR.RichTextField(blank=True)
-    interpretability = MDR.RichTextField(blank=True)
-    relevance = MDR.RichTextField(blank=True)
-    accuracy = MDR.RichTextField(blank=True)
-    coherence = MDR.RichTextField(blank=True)
-
-
-class Framework(MDR.concept):
-    template = "comet/framework.html"
-    # parentFramework = ConceptForeignKey('Framework', blank=True, null=True, related_name="childFrameworks")
-
-    serialize_weak_entities = [
-        ('dimensions', 'frameworkdimension_set'),
-    ]
-
-    def root_dimensions(self):
-        return self.frameworkdimension_set.all().filter(
-            parent=None
-        )
-
-
-class FrameworkDimension(MPTTModel, TimeStampedModel, aristotleComponent):
-    parent_field_name = 'framework'
-
-    objects = FrameworkDimensionManager()
-    framework = ConceptForeignKey('Framework', on_delete=models.CASCADE)
-    name = models.CharField(max_length=2048)
-    description = MDR.RichTextField(blank=True)
-    parent = TreeForeignKey(
-        'self',
-        null=True,
-        blank=True,
-        to_field='uuid',
-        on_delete=models.CASCADE,
-    )
-
-    class MPTTMeta:
-        order_insertion_by = ['name']
-
-    @property
-    def parentItem(self):
-        return self.framework
-
-    def __str__(self):
-        return self.name
