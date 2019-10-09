430047e813b0fa96356bca0f291c79b42bea5dd4
Francisco Gallardo <fragalcer@gmail.com>
2019-10-09 18:45:04 +1100

    Improved URLS Hacktoberfest.

--- docker/urls.py
+++ docker/urls.py
@@ -1,24 +1,24 @@
 from django.conf import settings
-from django.conf.urls import include, url
+from django.urls import path, include
 
 
 urlpatterns = [
-    url(r'^alias/', include('impersonate.urls')),
-    url(r'^api/', include('aristotle_mdr_api.urls')),
-    url(r'^', include(('aristotle_dse.urls', "aristotle_dse"), namespace="aristotle_dse")),
-    url(r'^', include(('aristotle_mdr.contrib.links.urls', "aristotle_mdr_links"), namespace="aristotle_mdr_links")),
-    url(r'^', include(('aristotle_mdr.contrib.slots.urls', "aristotle_slots"), namespace="aristotle_slots")),
-    url(r'^', include(('aristotle_mdr.contrib.identifiers.urls', "aristotle_mdr_identifiers"), namespace="aristotle_identifiers")),
-    url(r'^', include('aristotle_mdr.urls')),
-    url(r'^', include(('aristotle_mdr.contrib.links.urls', "aristotle_mdr_links"), namespace="aristotle_mdr_links")),
-    url(r'^comet/', include(('comet.urls', "comet"), namespace='comet')),
-    url(r'^glossary/', include(('aristotle_glossary.urls', "aristotle_glossary"), namespace="aristotle_glossary")),
+    path('alias/', include('impersonate.urls')),
+    path('api/', include('aristotle_mdr_api.urls')),
+    path('', include(('aristotle_dse.urls', "aristotle_dse"), namespace="aristotle_dse")),
+    path('', include(('aristotle_mdr.contrib.links.urls', "aristotle_mdr_links"), namespace="aristotle_mdr_links")),
+    path('', include(('aristotle_mdr.contrib.slots.urls', "aristotle_slots"), namespace="aristotle_slots")),
+    path('', include(('aristotle_mdr.contrib.identifiers.urls', "aristotle_mdr_identifiers"), namespace="aristotle_identifiers")),
+    path('', include('aristotle_mdr.urls')),
+    path('', include(('aristotle_mdr.contrib.links.urls', "aristotle_mdr_links"), namespace="aristotle_mdr_links")),
+    path('comet/', include(('comet.urls', "comet"), namespace='comet')),
+    path('glossary/', include(('aristotle_glossary.urls', "aristotle_glossary"), namespace="aristotle_glossary")),
 ]
 
 
 if settings.DEBUG and settings.DEBUG_TOOLBAR:
     import debug_toolbar
-    urlpatterns.append(url(r'^__debug__/', include(debug_toolbar.urls)))
+    urlpatterns.append(path('__debug__/', include(debug_toolbar.urls)))
 
 
 # This is only for dev work, so we can skip it.
@@ -26,7 +26,7 @@ if settings.DEBUG:  # pragma: no cover
     from django.contrib.staticfiles import views
 
     urlpatterns += [
-        url(r'^static/(?P<path>.*)$', views.serve),
+        path('static/<int:path>', views.serve),
     ]
 
     from django.conf.urls.static import static
