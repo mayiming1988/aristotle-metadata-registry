48d08ed3888144f570cbea4746eb43ac6186075d
Francisco Gallardo <fragalcer@gmail.com>
2019-10-18 17:27:51 +1100

    Several improvements.

--- python/aristotle-metadata-registry/aristotle_mdr/urls/aristotle.py
+++ python/aristotle-metadata-registry/aristotle_mdr/urls/aristotle.py
@@ -83,106 +83,105 @@ urlpatterns = [
 
     # Concept page overrides
     path('item/<int:iid>/dataelement/<path:name_slug>/', views.DataElementView.as_view(), name='dataelement'),
-    path('item/(?P<iid>\d+)/objectclass/<path:name_slug>/', views.ObjectClassView.as_view(), name='objectclass_view'),
+    path('item/<int:iid>/objectclass/<path:name_slug>/', views.ObjectClassView.as_view(), name='objectclass_view'),
     re_path(r'^item/(?P<iid>\d+)(?:/(?P<model_slug>\w+)/(?P<name_slug>.+))?/?$', views.ConceptView.as_view(), name='item'),
-    path(r'^item/(?P<iid>\d+)(?:/.*)?$', views.ConceptView.as_view(), name='item_short'),  # Catch every other 'item' URL and throw it for a redirect
-    path(r'^item/(?P<uuid>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/?(.*)?$', views.concept_by_uuid, name='item_uuid'),
+    re_path(r'^item/(?P<iid>\d+)(?:/.*)?$', views.ConceptView.as_view(), name='item_short'),  # Catch every other 'item' URL and throw it for a redirect
+    path('item/<uuid:uuid>/?<path>?$', views.concept_by_uuid, name='item_uuid'),
 
-    path(r'^unmanaged/measure/(?P<iid>\d+)(?:/(?P<model_slug>\w+)/(?P<name_slug>.+))?/?$', views.MeasureView.as_view(), name='measure'),
-    path(r"^managed_items/(?P<model_slug>.+)/(?P<iid>.+)?$", view=views.ManagedItemView.as_view(), name="view_managed_item"),
+    re_path(r'^unmanaged/measure/(?P<iid>\d+)(?:/(?P<model_slug>\w+)/(?P<name_slug>.+))?/?$', views.MeasureView.as_view(), name='measure'),
+    path('managed_items/<path:model_slug>/<path:iid>', view=views.ManagedItemView.as_view(), name='view_managed_item'),
 
-    # path(r'^create/?$', views.item, name='item'),
-    path(r'^create/?$', views.create_list, name='create_list'),
-    path(r'^create/wizard/aristotle_mdr/dataelementconcept$', views.wizards.DataElementConceptWizard.as_view(), name='createDataElementConcept'),
-    path(r'^create/wizard/aristotle_mdr/dataelement$', views.wizards.DataElementWizard.as_view(), name='createDataElement'),
-    path(r'^create/(?P<app_label>.+)/(?P<model_name>.+)/?$', views.wizards.create_item, name='createItem'),
-    path(r'^create/(?P<model_name>.+)/?$', views.wizards.create_item, name='createItem'),
+    # path('create/', views.item, name='item'),
+    path('create/', views.create_list, name='create_list'),
+    path('create/wizard/aristotle_mdr/dataelementconcept', views.wizards.DataElementConceptWizard.as_view(), name='createDataElementConcept'),
+    path('create/wizard/aristotle_mdr/dataelement', views.wizards.DataElementWizard.as_view(), name='createDataElement'),
+    path('create/<path:app_label>/<path:model_name>/', views.wizards.create_item, name='createItem'),
+    path('create/<path:model_name>/', views.wizards.create_item, name='createItem'),
 
-    path(r'^download/options/(?P<download_type>\w+)/?$', views.downloads.DownloadOptionsView.as_view(), name='download_options'),
-    path(r'^download/bulk/(?P<download_type>\w+)/?$', views.downloads.BulkDownloadView.as_view(), name='bulk_download'),
-    path(r'^download/(?P<download_type>\w+)/(?P<iid>\d+)/?$', views.downloads.DownloadView.as_view(), name='download'),
-    path(r'^dlstatus/(?P<taskid>[a-z0-9\-]+)/?$',
-         views.downloads.DownloadStatusView.as_view(),
-         name='download_status',
+    path('download/options/<slug:download_type>/', views.downloads.DownloadOptionsView.as_view(), name='download_options'),
+    path('download/bulk/<slug:download_type>/', views.downloads.BulkDownloadView.as_view(), name='bulk_download'),
+    path('download/<slug:download_type>/<int:iid>/', views.downloads.DownloadView.as_view(), name='download'),
+    re_path('dlstatus/(?P<taskid>[a-z0-9\-]+)/?$',
+            views.downloads.DownloadStatusView.as_view(),
+            name='download_status',
+            ),
+
+    path('action/supersede/<int:iid>', views.actions.SupersedeItemView.as_view(), name='supersede'),
+    path('action/proposed/supersede/<int:iid>', views.actions.ProposedSupersedeItemView.as_view(), name='proposed_supersede'),
+
+    path('action/bulkaction/', views.bulk_actions.BulkAction.as_view(), name='bulk_action'),
+    path('action/bulkaction/state/', views.bulk_actions.ChangeStatusBulkActionView.as_view(), name='change_state_bulk_action'),
+    path('action/changestatus/<int:iid>', views.ChangeStatusView.as_view(), name='changeStatus'),
+    path('toolbox/compare/', views.comparator.MetadataComparison.as_view(), name='compare_concepts'),
+    path('toolbox/dataelementcomponents/', views.tools.DataElementsAndSubcomponentsStatusCheckTool.as_view(), name='data_element_components_tool'),
+
+    path('status/delete/<int:sid>/item/<int:iid>', views.DeleteStatus.as_view(), name='deleteStatus'),
+    path('status/edit/<int:sid>/item/<int:iid>/registrationauthority/<slug:raid>', views.EditStatus.as_view(), name='editStatus'),
+    path('status/history/<int:sid>/item/<int:iid>/registrationauthority/<slug:raid>', views.StatusHistory.as_view(), name='statusHistory'),
+
+    path('account/', RedirectView.as_view(url=reverse_lazy("aristotle:userHome"), permanent=True)),
+    path('account/home/', views.user_pages.home, name='userHome'),
+    path('account/sandbox/', views.user_pages.SandboxedItemsView.as_view(), name='userSandbox'),
+    path('account/sandbox/delete/', views.actions.DeleteSandboxView.as_view(), name="sandbox_delete"),
+    path('account/roles/', views.user_pages.Roles.as_view(), name='userRoles'),
+    path('account/admin/', views.user_pages.admin_tools, name='userAdminTools'),
+    path('account/admin/statistics/', views.user_pages.admin_stats, name='userAdminStats'),
+    path('account/edit/', views.user_pages.EditView.as_view(), name='userEdit'),
+    path('account/notificationpermissions/', views.user_pages.NotificationPermissions.as_view(), name='notificationPermissions'),
+    path('account/profile/', views.user_pages.ProfileView.as_view(), name='userProfile'),
+    path('account/recent/', views.user_pages.recent, name='userRecentItems'),
+    path('account/workgroups/', views.user_pages.MyWorkgroupList.as_view(), name='userWorkgroups'),
+    path('account/workgroups/archives/', views.user_pages.WorkgroupArchiveList.as_view(), name='user_workgroups_archives'),
+    path('account/notifications/', views.user_pages.InboxView.as_view(), name='userInbox'),
+    path('account/notifications-all/?$', views.user_pages.InboxViewAll.as_view(), name='userInboxAll'),
+    path('account/notifications/api/mark-all-as-read/', views.notify.MarkAllReadApiView.as_view(), name='api_mark_all_read'),
+
+    re_path(r'^account/django/(.*)?$', views.user_pages.django_admin_wrapper, name='django_admin'),
+
+    path('account/registrartools/', views.user_pages.RegistrarTools.as_view(), name='userRegistrarTools'),
+
+    path('registrationauthority/create/', views.registrationauthority.CreateRegistrationAuthority.as_view(), name='registrationauthority_create'),
+    path('account/admin/registrationauthority/all/', views.registrationauthority.ListRegistrationAuthorityAll.as_view(), name='registrationauthority_list'),
+
+    re_path(r'^registrationauthority/(?P<iid>\d+)(?:/(?P<name_slug>.+))?/data_dictionary$', views.registrationauthority.DateFilterView.as_view(), name='registrationauthority_data_dictionary'),
+    re_path(r'^registrationauthority/(?P<iid>\d+)(?:/(?P<name_slug>.+))?/data_dictionary/(?P<download_type>\w+)/(?P<state_name>\w+)/(?P<registration_date>.+)$', views.registrationauthority.DataDictionaryDownloadOptionsView.as_view(), name='registrationauthority_data_dictionary_download_options'),
+    re_path(r'^registrationauthority/(?P<iid>\d+)(?:/(?P<name_slug>.+))?/members', views.registrationauthority.MembersRegistrationAuthority.as_view(), name='registrationauthority_members'),
+    re_path(r'^registrationauthority/(?P<iid>\d+)(?:/(?P<name_slug>.+))?/edit', views.registrationauthority.EditRegistrationAuthority.as_view(), name='registrationauthority_edit'),
+    re_path(r'^registrationauthority/(?P<iid>\d+)(?:/(?P<name_slug>.+))?/states', views.registrationauthority.EditRegistrationAuthorityStates.as_view(), name='registrationauthority_edit_states'),
+    re_path(r'^registrationauthority/(?P<iid>\d+)(?:/(?P<name_slug>.+))?/rules', views.registrationauthority.RAValidationRuleEditView.as_view(), name='registrationauthority_rules'),
+    re_path(r'^registrationauthority/(?P<iid>\d+)(?:/(?P<name_slug>.+))?/add_user/?$', views.registrationauthority.AddUser.as_view(), name='registrationauthority_add_user'),
+    re_path(r'^registrationauthority/(?P<iid>\d+)(?:/(?P<name_slug>.+))?/change_roles/(?P<user_pk>\d+)?/?$', views.registrationauthority.ChangeUserRoles.as_view(), name='registrationauthority_change_user_roles'),
+    re_path(r'^registrationauthority/(?P<iid>\d+)(?:/(?P<name_slug>.+))?/remove/(?P<user_pk>\d+)/?$', views.registrationauthority.RemoveUser.as_view(), name='registrationauthority_member_remove'),
+    re_path(r'^registrationauthority/(?P<iid>\d+)(?:/(?P<name_slug>.+))?/', views.registrationauthority.RegistrationAuthorityView.as_view(), name='registrationAuthority'),
+
+    re_path(r'^organization/(?P<iid>\d+)?(?:/(?P<name_slug>.+))?/?$', views.registrationauthority.organization, name='organization'),
+    path('organizations/', views.registrationauthority.all_organizations, name='all_organizations'),
+    path('registrationauthorities/', views.registrationauthority.all_registration_authorities, name='all_registration_authorities'),
+
+    path('extensions/', views.extensions, name='extensions'),
+
+    path('notifyredirect/<int:content_type>/<int:object_id>/', views.notification_redirect, name="notify_redirect"),
+
+    path('about/aristotle/', TemplateView.as_view(template_name='aristotle_mdr/static/aristotle_mdr.html'), name="aboutMain"),
+    path('about/<slug:template>/', views.DynamicTemplateView.as_view(), name="about"),
+
+    path('accessibility/', TemplateView.as_view(template_name='aristotle_mdr/static/accessibility.html'), name="accessibility"),
+
+    path('user/<int:uid>/profilePicture', views.user_pages.profile_picture, name="profile_picture"),
+    path('user/<int:uid>/profilePicture.svg', views.user_pages.profile_picture, name="dynamic_profile_picture"),
+
+    path('share/<slug:share>', views.user_pages.SharedSandboxView.as_view(), name='sharedSandbox'),
+    path('share/<slug:share>/<int:iid>', views.user_pages.SharedItemView.as_view(), name='sharedSandboxItem'),
+
+    path('version/<int:verid>', views.versions.ConceptVersionView.as_view(), name='item_version'),
+
+    path('search/',
+         search_view_factory(
+             view_class=PermissionSearchView,
+             template='search/search.html',
+             searchqueryset=None,
+             form_class=forms.search.PermissionSearchForm
          ),
-
-    path(r'^action/supersede/(?P<iid>\d+)$', views.actions.SupersedeItemView.as_view(), name='supersede'),
-    path(r'^action/proposed/supersede/(?P<iid>\d+)$', views.actions.ProposedSupersedeItemView.as_view(), name='proposed_supersede'),
-
-    path(r'^action/bulkaction/?$', views.bulk_actions.BulkAction.as_view(), name='bulk_action'),
-    path(r'^action/bulkaction/state/?$', views.bulk_actions.ChangeStatusBulkActionView.as_view(), name='change_state_bulk_action'),
-    path(r'^action/changestatus/(?P<iid>\d+)$', views.ChangeStatusView.as_view(), name='changeStatus'),
-    path(r'^toolbox/compare/?$', views.comparator.MetadataComparison.as_view(), name='compare_concepts'),
-    path(r'^toolbox/dataelementcomponents/?$', views.tools.DataElementsAndSubcomponentsStatusCheckTool.as_view(), name='data_element_components_tool'),
-
-    path(r'^status/delete/(?P<sid>\d+)/item/(?P<iid>\d+)$', views.DeleteStatus.as_view(), name='deleteStatus'),
-    path(r'^status/edit/(?P<sid>\d+)/item/(?P<iid>\d+)/registrationauthority/(?P<raid>\w+)$', views.EditStatus.as_view(), name='editStatus'),
-    path(r'^status/history/(?P<sid>\d+)/item/(?P<iid>\d+)/registrationauthority/(?P<raid>\w+)$', views.StatusHistory.as_view(), name='statusHistory'),
-
-    path(r'^account/?$', RedirectView.as_view(url=reverse_lazy("aristotle:userHome"), permanent=True)),
-    path(r'^account/home/?$', views.user_pages.home, name='userHome'),
-    path(r'^account/sandbox/?$', views.user_pages.SandboxedItemsView.as_view(), name='userSandbox'),
-    path(r'^account/sandbox/delete/?$', views.actions.DeleteSandboxView.as_view(), name="sandbox_delete"),
-    path(r'^account/roles/?$', views.user_pages.Roles.as_view(), name='userRoles'),
-    path(r'^account/admin/?$', views.user_pages.admin_tools, name='userAdminTools'),
-    path(r'^account/admin/statistics/?$', views.user_pages.admin_stats, name='userAdminStats'),
-    path(r'^account/edit/?$', views.user_pages.EditView.as_view(), name='userEdit'),
-    path(r'^account/notificationpermissions/?$', views.user_pages.NotificationPermissions.as_view(), name='notificationPermissions'),
-    path(r'^account/profile/?$', views.user_pages.ProfileView.as_view(), name='userProfile'),
-    path(r'^account/recent/?$', views.user_pages.recent, name='userRecentItems'),
-    path(r'^account/workgroups/?$', views.user_pages.MyWorkgroupList.as_view(), name='userWorkgroups'),
-    path(r'^account/workgroups/archives/?$', views.user_pages.WorkgroupArchiveList.as_view(), name='user_workgroups_archives'),
-    path(r'^account/notifications/?$', views.user_pages.InboxView.as_view(), name='userInbox'),
-    path(r'^account/notifications-all/?$', views.user_pages.InboxViewAll.as_view(), name='userInboxAll'),
-    path(r'^account/notifications/api/mark-all-as-read/', views.notify.MarkAllReadApiView.as_view(), name='api_mark_all_read'),
-
-    path(r'^account/django/(.*)?$', views.user_pages.django_admin_wrapper, name='django_admin'),
-
-    path(r'^account/registrartools/?$', views.user_pages.RegistrarTools.as_view(), name='userRegistrarTools'),
-
-    path(r'^registrationauthority/create/?$', views.registrationauthority.CreateRegistrationAuthority.as_view(), name='registrationauthority_create'),
-    path(r'^account/admin/registrationauthority/all/?$', views.registrationauthority.ListRegistrationAuthorityAll.as_view(), name='registrationauthority_list'),
-
-    path(r'^registrationauthority/(?P<iid>\d+)(?:/(?P<name_slug>.+))?/data_dictionary$', views.registrationauthority.DateFilterView.as_view(), name='registrationauthority_data_dictionary'),
-    path(r'^registrationauthority/(?P<iid>\d+)(?:/(?P<name_slug>.+))?/data_dictionary/(?P<download_type>\w+)/(?P<state_name>\w+)/(?P<registration_date>.+)$', views.registrationauthority.DataDictionaryDownloadOptionsView.as_view(), name='registrationauthority_data_dictionary_download_options'),
-    path(r'^registrationauthority/(?P<iid>\d+)(?:/(?P<name_slug>.+))?/members', views.registrationauthority.MembersRegistrationAuthority.as_view(), name='registrationauthority_members'),
-    path(r'^registrationauthority/(?P<iid>\d+)(?:/(?P<name_slug>.+))?/edit', views.registrationauthority.EditRegistrationAuthority.as_view(), name='registrationauthority_edit'),
-    path(r'^registrationauthority/(?P<iid>\d+)(?:/(?P<name_slug>.+))?/states', views.registrationauthority.EditRegistrationAuthorityStates.as_view(), name='registrationauthority_edit_states'),
-    path(r'^registrationauthority/(?P<iid>\d+)(?:/(?P<name_slug>.+))?/rules', views.registrationauthority.RAValidationRuleEditView.as_view(), name='registrationauthority_rules'),
-    path(r'^registrationauthority/(?P<iid>\d+)(?:/(?P<name_slug>.+))?/add_user/?$', views.registrationauthority.AddUser.as_view(), name='registrationauthority_add_user'),
-    path(r'^registrationauthority/(?P<iid>\d+)(?:/(?P<name_slug>.+))?/change_roles/(?P<user_pk>\d+)?/?$', views.registrationauthority.ChangeUserRoles.as_view(), name='registrationauthority_change_user_roles'),
-    path(r'^registrationauthority/(?P<iid>\d+)(?:/(?P<name_slug>.+))?/remove/(?P<user_pk>\d+)/?$', views.registrationauthority.RemoveUser.as_view(), name='registrationauthority_member_remove'),
-    path(r'^registrationauthority/(?P<iid>\d+)(?:/(?P<name_slug>.+))?/', views.registrationauthority.RegistrationAuthorityView.as_view(), name='registrationAuthority'),
-
-    path(r'^organization/(?P<iid>\d+)?(?:/(?P<name_slug>.+))?/?$', views.registrationauthority.organization, name='organization'),
-    path(r'^organizations/?$', views.registrationauthority.all_organizations, name='all_organizations'),
-    path(r'^registrationauthorities/?$', views.registrationauthority.all_registration_authorities, name='all_registration_authorities'),
-
-    path(r'^extensions/?$', views.extensions, name='extensions'),
-
-    path(r'^notifyredirect/(?P<content_type>\d+)/(?P<object_id>\d+)/', views.notification_redirect, name="notify_redirect"),
-
-    path(r'^about/aristotle/?$', TemplateView.as_view(template_name='aristotle_mdr/static/aristotle_mdr.html'), name="aboutMain"),
-    path(r'^about/(?P<template>.+)/?$', views.DynamicTemplateView.as_view(), name="about"),
-
-    path(r'^accessibility/?$', TemplateView.as_view(template_name='aristotle_mdr/static/accessibility.html'), name="accessibility"),
-
-    path(r'user/(?P<uid>\d+)/profilePicture', views.user_pages.profile_picture, name="profile_picture"),
-    path(r'user/(?P<uid>\d+)/profilePicture.svg', views.user_pages.profile_picture, name="dynamic_profile_picture"),
-
-    path(r'share/(?P<share>[\w-]+)$', views.user_pages.SharedSandboxView.as_view(), name='sharedSandbox'),
-    path(r'share/(?P<share>[\w-]+)/(?P<iid>\d+)', views.user_pages.SharedItemView.as_view(), name='sharedSandboxItem'),
-
-    path(r'version/(?P<verid>\d+)', views.versions.ConceptVersionView.as_view(), name='item_version'),
-
-    path(
-        r'^search/?$',
-        search_view_factory(
-            view_class=PermissionSearchView,
-            template='search/search.html',
-            searchqueryset=None,
-            form_class=forms.search.PermissionSearchForm
-        ),
-        name='search'
-    ),
+         name='search'
+         ),
 ]
