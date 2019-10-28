6a4dff21bc82e44e0c069dbdc634c4dfef9d1219
Dylan Silke <d.ylan.s.silke@gmail.com>
2019-10-28 11:33:25 +1100

    Fixes error where versions were failing when a inclusion in an indicator was None

--- python/comet-indicator-registry/comet/tests/settings.py
+++ python/comet-indicator-registry/comet/tests/settings.py
@@ -1,11 +1,4 @@
 from aristotle_mdr.tests.settings.settings import *
-from aristotle_mdr.required_settings import INSTALLED_APPS
-
-INSTALLED_APPS = (
-    #The good stuff
-    'comet',
-    'aristotle_dse',
-) + INSTALLED_APPS
 
 ARISTOTLE_SETTINGS['CONTENT_EXTENSIONS'] = ['comet']
 
