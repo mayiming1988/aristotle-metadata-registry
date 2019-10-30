6d935a1763c9f889c03783fc04b10234114e057f
Dylan Silke <d.ylan.s.silke@gmail.com>
2019-10-30 15:29:30 +1100

    Fixed test

--- python/aristotle-metadata-registry/aristotle_mdr/tests/main/test_versions.py
+++ python/aristotle-metadata-registry/aristotle_mdr/tests/main/test_versions.py
@@ -644,20 +644,24 @@ class CheckStatusHistoryReversionTests(utils.AristotleTestUtils, TestCase):
     def test_status_reversion_page_only_visible_to_superusers(self):
         """Test that the status reversion page 403s for any user but superusers"""
         self.logout()
+        url = reverse('friendly_login') + '?next=' + reverse('aristotle:statusHistory', args=[self.status.id,
+                                                                                              self.object_class.id,
+                                                                                              self.ra.id])
         response = self.client.get(
-            reverse('aristotle:statusHistory', args=[self.status.id, self.object_class.id, self.ra.id]))
-        self.assertEqual(response.status_code, 403)
+            reverse('aristotle:statusHistory', args=[self.status.id, self.object_class.id, self.ra.id])
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, url)
 
         self.login_viewer()
         response = self.client.get(
             reverse('aristotle:statusHistory', args=[self.status.id, self.object_class.id, self.ra.id]))
         self.assertEqual(response.status_code, 403)
-        self.logout()
 
         self.login_regular_user()
         response = self.client.get(
             reverse('aristotle:statusHistory', args=[self.status.id, self.object_class.id, self.ra.id]))
-        self.assertEqual((response.status_code, 403))
+        self.assertEqual(response.status_code, 403)
         self.logout()
 
         self.login_editor()
