0501801c9f3c56aaaab2ca3cfe41b2cd2ea022d8
Francisco Gallardo <fragalcer@gmail.com>
2019-11-11 16:25:01 +1100

    Fixed urls.

--- python/aristotle-mdr-api/aristotle_mdr_api/v4/reviews/urls.py
+++ python/aristotle-mdr-api/aristotle_mdr_api/v4/reviews/urls.py
@@ -1,10 +1,10 @@
-from django.urls import re_path
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    re_path(r'(?P<pk>\d+)/promote-concept/$', views.PromoteImpactedItemToReviewItemsView.as_view(), name='promote_concept'),
-    re_path(r'(?P<pk>\d+)/remove-concept/$', views.RemoveItemFromReviewItemsView.as_view(), name='remove_concept'),
-    re_path(r'(?P<pk>\d+)/updatecomment/$', views.ReviewUpdateAndCommentView.as_view(), name='update_and_comment'),
-    re_path(r'comments/$', views.ReviewCommentCreateView.as_view(), name='comment'),
-    re_path(r'comments/(?P<pk>\d+)/$', views.ReviewCommentRetrieveView.as_view(), name='comment_get'),
+    path('comments/', views.ReviewCommentCreateView.as_view(), name='comment'),
+    path('comments/<int:pk>/', views.ReviewCommentRetrieveView.as_view(), name='comment_get'),
+    path('<int:pk>/promote-concept/', views.PromoteImpactedItemToReviewItemsView.as_view(), name='promote_concept'),
+    path('<int:pk>/remove-concept/', views.RemoveItemFromReviewItemsView.as_view(), name='remove_concept'),
+    path('<int:pk>/update-comment/', views.ReviewUpdateAndCommentView.as_view(), name='update_and_comment'),
 ]
