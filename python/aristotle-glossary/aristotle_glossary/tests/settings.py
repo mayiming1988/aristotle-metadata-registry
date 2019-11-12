588c3684234547f29e469bcbcbe659ff6a27677b
Dylan Silke <d.ylan.s.silke@gmail.com>
2019-11-13 10:37:09 +1100

    Added method to serialize subcomponents during loading

--- python/aristotle-glossary/aristotle_glossary/tests/settings.py
+++ python/aristotle-glossary/aristotle_glossary/tests/settings.py
@@ -4,9 +4,11 @@ from aristotle_mdr.required_settings import INSTALLED_APPS
 INSTALLED_APPS = (
     # The good stuff
     'aristotle_glossary',
+    'aristotle_dse'
 ) + INSTALLED_APPS
 
 ARISTOTLE_SETTINGS['SEPARATORS']['DataElementConcept'] = '--'
-ARISTOTLE_SETTINGS['CONTENT_EXTENSIONS'] = ARISTOTLE_SETTINGS['CONTENT_EXTENSIONS'] +['aristotle_glossary']
+ARISTOTLE_SETTINGS['CONTENT_EXTENSIONS'] = ARISTOTLE_SETTINGS['CONTENT_EXTENSIONS'] +\
+    ['aristotle_glossary', 'aristotle_dse']
 
 ROOT_URLCONF = 'aristotle_glossary.tests.urls'
