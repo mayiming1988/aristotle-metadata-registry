3ef8e0521220186da69be9c54a08c2fdd09a0a44
fragalcer <fragalcer@gmail.com>
2019-11-13 22:27:16 +1100

    Fixed urls

--- python/aristotle-metadata-registry/aristotle_mdr/contrib/identifiers/urls.py
+++ python/aristotle-metadata-registry/aristotle_mdr/contrib/identifiers/urls.py
@@ -3,7 +3,7 @@ from aristotle_mdr.contrib.identifiers.views import scoped_identifier_redirect, 
 
 
 urlpatterns = [
-    path('identifier/<str:ns_prefix>/<int:iid>/<str:version>', scoped_identifier_redirect, name='scoped_identifier_redirect_version'),
-    path('identifier/<str:ns_prefix>/<int:iid>', scoped_identifier_redirect, name='scoped_identifier_redirect'),
+    path('identifier/<str:ns_prefix>/<str:identifier>/<str:version>', scoped_identifier_redirect, name='scoped_identifier_redirect_version'),
+    path('identifier/<str:ns_prefix>/<str:identifier>', scoped_identifier_redirect, name='scoped_identifier_redirect'),
     path('identifier/<str:ns_prefix>', namespace_redirect, name='namespace_redirect'),
 ]
