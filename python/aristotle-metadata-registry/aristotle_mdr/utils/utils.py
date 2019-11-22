36e04453b9a5c49765330ce1a9365e656e344da8
Dylan Silke <d.ylan.s.silke@gmail.com>
2019-11-22 11:00:37 +1100

    Recent actions: further reversion of .format

--- python/aristotle-metadata-registry/aristotle_mdr/utils/utils.py
+++ python/aristotle-metadata-registry/aristotle_mdr/utils/utils.py
@@ -180,13 +180,10 @@ def construct_change_message(form, formsets):
         for formset in formsets:
             if formset.model:
                 for added_object in formset.new_objects:
-                    messages_list.append(
-                        _('Added %(name)s "%(object)s".').format(
-                            name=force_text(added_object._meta.verbose_name),
-                            object=force_text(added_object),
-                        )
-                    )
-
+                    # Translators: A message in the version history of an item saying that an object with the name (name) of the type (object) has been created in the registry.
+                    messages_list.append(_('Added %(name)s "%(object)s".')
+                                          % {'name': force_text(added_object._meta.verbose_name),
+                                             'object': force_text(added_object)})
                 for changed_object, changed_fields in formset.changed_objects:
                     messages_list.append(
                         _('Changed %(list)s for %(name)s "%(object)s".').format(
@@ -196,11 +193,10 @@ def construct_change_message(form, formsets):
                     )
 
                 for deleted_object in formset.deleted_objects:
-                    messages_list.append(
-                        _('Deleted {name} "{object}s".').format(
-                            name=force_text(deleted_object._meta.verbose_name),
-                            object=force_text(deleted_object)),
-                    )
+                    # Translators: A message in the version history of an item saying that an object with the name (name) of the type (object) has been deleted from the registry.
+                    messages_list.append(_('Deleted %(name)s "%(object)s".')
+                                          % {'name': force_text(deleted_object._meta.verbose_name),
+                                             'object': force_text(deleted_object)})
 
     change_message = ', '.join(messages_list)
     return change_message or _('No fields changed.')
