8943512c081d1aa3091ed3e4b7526415c975eeca
Francisco Gallardo <fragalcer@gmail.com>
2019-11-25 17:18:13 +1100

    Fixed github actions.

--- python/aristotle-metadata-registry/aristotle_mdr/contrib/links/urls.py
+++ python/aristotle-metadata-registry/aristotle_mdr/contrib/links/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.urls import path
 from django.utils.translation import ugettext_lazy as _
 
 from aristotle_mdr.contrib.generic.views import (
@@ -9,20 +10,18 @@ from aristotle_mdr.contrib.links import models, views
 
 
 urlpatterns = [
-    url(r'^relation/(?P<iid>\d+)?/relation/edit/roles/?$',
-        GenericAlterOneToManyView.as_view(
-            model_base=models.Relation,
-            model_to_add=models.RelationRole,
-            model_base_field='relationrole_set',
-            model_to_add_field='relation',
-            ordering_field='ordinal',
-            form_add_another_text=_('Add a role'),
-            form_title=_('Change Relation Roles'),
-        ), name='relation_roles_edit'),
-    url(r'^relation/link/(?P<iid>\d+)/add/$',
-        views.AddLinkWizard.as_view(), name='add_link'),
-    url(r'^relation/link/edit/(?P<linkid>\d+)$',
-        views.EditLinkFormView.as_view(), name='edit_link'),
-    url(r'^relation/link/json/(?P<iid>\d+)$',
-        views.link_json_for_item, name='link_json_for_item'),
+    path('relation/<int:iid>/relation/edit/roles/',
+         GenericAlterOneToManyView.as_view(
+             model_base=models.Relation,
+             model_to_add=models.RelationRole,
+             model_base_field='relationrole_set',
+             model_to_add_field='relation',
+             ordering_field='ordinal',
+             form_add_another_text=_('Add a role'),
+             form_title=_('Change Relation Roles'),
+         ), name='relation_roles_edit'),
+    path('relation/link/<int:iid>/add/', views.AddLinkWizard.as_view(), name='add_link'),
+    path('relation/link/edit/<int:linkid>', views.EditLinkFormView.as_view(), name='edit_link'),
+    path('relation/link/json/<int:iid>', views.link_json_for_item, name='link_json_for_item'),
+    path('relation/<int:iid>/link/<int:linkid>/remove-link/', views.remove_link_for_item, name='remove_link')
 ]
