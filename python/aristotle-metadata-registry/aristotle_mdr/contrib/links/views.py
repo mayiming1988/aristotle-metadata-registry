d7a637b7e488b28e36295211f35d3abdca4046f9
Dylan Silke <d.ylan.s.silke@gmail.com>
2019-11-27 13:54:35 +1100

    Chore: removed uneeded import

--- python/aristotle-metadata-registry/aristotle_mdr/contrib/links/views.py
+++ python/aristotle-metadata-registry/aristotle_mdr/contrib/links/views.py
@@ -3,7 +3,6 @@ from django.urls import reverse
 from django.db import transaction
 from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import redirect, get_object_or_404
-from django.contrib.auth.decorators import login_required
 from django.views.generic import FormView
 
 from aristotle_mdr import models as MDR
