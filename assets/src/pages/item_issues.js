17ff2f6ca0614cafa97cd2cc2692f28ba0bc5eea
Dylan Silke <d.ylan.s.silke@gmail.com>
2019-12-03 10:38:01 +1100

    UI: added swappable panels rather than double tabs

--- assets/src/pages/item_issues.js
+++ assets/src/pages/item_issues.js
@@ -5,5 +5,32 @@ import Vue from 'vue'
 import rootComponent from '@/root/item_issues.js'
 
 
-initCore()
-new Vue(rootComponent)
+initCore();
+new Vue(rootComponent);
+
+for (let button of document.getElementsByClassName('swap-panels')) {
+    button.addEventListener('click', () => {
+        // Extract the data-target of which button was clicked
+        let container = document.querySelector('#swap-container');
+        let associatedElement = container.querySelector(event.target.dataset.target);
+
+        // Add an active class to the clicked buttons
+        for (let button of container.querySelectorAll('.swap-panels')) {
+            if (event.target === button) {
+                button.classList.add('active');
+            }
+            else {
+                button.classList.remove('active');
+            }
+        }
+        // Collapse all of them except the one that corresponds to the one that was clicked
+        for (let collapsiblePanel of container.querySelectorAll('div.collapse')) {
+            if (collapsiblePanel !== associatedElement) {
+                collapsiblePanel.classList.remove('in');
+            }
+            else {
+                collapsiblePanel.classList.add('in');
+            }
+        }
+    });
+}
