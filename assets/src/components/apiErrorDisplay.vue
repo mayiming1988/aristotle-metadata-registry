ff90602406b838bae934f8d3782e2c077de45d86
Francisco Gallardo <fragalcer@gmail.com>
2020-01-17 06:50:36 +1100

    Style: Fixed indentation.

--- assets/src/components/apiErrorDisplay.vue
+++ assets/src/components/apiErrorDisplay.vue
@@ -1,26 +1,26 @@
 <template>
-  <div class="api-errors">
-    <div v-for="(errlist, key) in errors" :key="key" class="alert alert-danger" role="alert">
-      <strong>{{ key }}:</strong>
-      <ul v-if="errlist.length > 1">
-        <li v-for="err in errlist" :key="err">
-          {{ err }}
-        </li>
-      </ul>
-      <template v-else>
-        {{ errlist[0] }}
-      </template>
+    <div class="api-errors">
+        <div v-for="(errlist, key) in errors" :key="key" class="alert alert-danger" role="alert">
+            <strong>{{ key }}:</strong>
+            <ul v-if="errlist.length > 1">
+                <li v-for="err in errlist" :key="err">
+                    {{ err }}
+                </li>
+            </ul>
+            <template v-else>
+                {{ errlist[0] }}
+            </template>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-// Displays all errors in one place
-import { capitalize } from 'src/lib/utils.js'
-export default {
-    props: ['errors'],
-    methods: {
-        capitalize: capitalize
+    // Displays all errors in one place
+    import { capitalize } from 'src/lib/utils.js'
+    export default {
+        props: ['errors'],
+        methods: {
+            capitalize: capitalize
+        }
     }
-}
 </script>
