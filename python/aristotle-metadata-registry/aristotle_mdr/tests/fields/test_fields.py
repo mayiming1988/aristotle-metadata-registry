75bc52b0c778cd97c394c34bf2d1ae944eeaab7c
Dylan Silke <d.ylan.s.silke@gmail.com>
2020-01-21 09:08:58 +1100

    Testing: added beginning of improved formal testing framework

--- python/aristotle-metadata-registry/aristotle_mdr/tests/fields/test_fields.py
+++ python/aristotle-metadata-registry/aristotle_mdr/tests/fields/test_fields.py
@@ -1,7 +1,8 @@
 from aristotle_mdr.tests.utils import AristotleTestUtils
 from aristotle_mdr.utils.utils import get_concept_models
 from aristotle_mdr.contrib.serializers.utils import get_concept_fields, get_relation_field_names
-from django.test import TestCase
+
+from django.test import TestCase, override_settings
 
 from ddf import G  # Django Dynamic Fixture
 
@@ -22,33 +23,42 @@ def generate_item_test(model):
         self.assertEqual(response.status_code, self.OK)
 
         # Check that all the concept (the fields that are directly on the concept) fields appear with their content
+        failures = []
         for field in get_concept_fields(model):
             value = field.value_from_object(item)
             field_name = field.name
-            self.assertContains(response, value)
-            self.assertContains(response, field_name)
+            if str(value) not in response:
+                failures.append(f"Can't find field value: {value} in response")
+            if field_name not in response:
+                failures.append(f"Can't find field_name: {field_name} in response")
 
-        # Check that the relation field headings appear (this is only the name, the testing of the actual fields needs
-        # to be tested separately
-        for field_name in get_relation_field_names(model):
-            self.assertContains(response, field_name)
+        if failures:
+            for failure in failures:
+                print(failure)
+            raise AssertionError
 
-    test.__doc__ = f'Test that all fields are visible on the item page for {model}'
+        # # Check that the relation field headings appear (this is only the name, the testing of the actual fields needs
+        # # to be tested separately
+        # for field_name in get_relation_field_names(model):
+        #     self.assertContains(response, field_name)
+
+    test.__doc__ = f'Test that all fields are visible on the item page for {model.__name__}'
     return test
 
 
 class FieldsMetaclass(type):
     def __new__(mcs, name, bases, attrs):
         # Iterate over all concept models, creating one test per model
-        metaclass = super().__new__(name, bases, attrs)
+        metaclass = super().__new__(mcs, name, bases, attrs)
 
         for model in get_concept_models():
             test = generate_item_test(model)
-            setattr(metaclass, f'test_all_fields_appear_on_{model}', test)
+            setattr(metaclass, f'test_all_fields_appear_on_{model.__name__}', test)
 
         return metaclass
 
 
+@override_settings(DDF_FILL_NULLABLE_FIELDS=True)
 class FieldsTestCase(AristotleTestUtils, TestCase, metaclass=FieldsMetaclass):
     """A class to formally check that fields appear on the item page"""
 
