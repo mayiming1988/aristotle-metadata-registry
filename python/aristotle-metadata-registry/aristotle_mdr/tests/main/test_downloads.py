62d7d311be0e4e1e3128a0c98c4fdf9b62ac567c
Dylan Silke <d.ylan.s.silke@gmail.com>
2020-01-23 13:49:59 +1100

    Fix: reverted changes to decorators

--- python/aristotle-metadata-registry/aristotle_mdr/tests/main/test_downloads.py
+++ python/aristotle-metadata-registry/aristotle_mdr/tests/main/test_downloads.py
@@ -291,8 +291,7 @@ class DownloaderTestCase(AristotleTestUtils, TestCase):
         self.assertTrue(expected_regen_url in message.body)
 
 
-@override_settings(
-    ARISTOTLE_SETTINGS={'DOWNLOAD_OPTIONS': {'DOWNLOADERS': ['aristotle_mdr.downloaders.HTMLDownloader']}})
+@override_settings(ARISTOTLE_SETTINGS={'DOWNLOAD_OPTIONS': {'DOWNLOADERS': ['aristotle_mdr.downloaders.HTMLDownloader']}})
 class TestHTMLDownloader(AristotleTestUtils, TestCase):
 
     def setUp(self):
@@ -425,8 +424,7 @@ class TestHTMLDownloader(AristotleTestUtils, TestCase):
         self.assertFalse('You dont have permission' in html)
 
 
-@override_settings(
-    ARISTOTLE_SETTINGS={'DOWNLOAD_OPTIONS': {'DOWNLOADERS': ['aristotle_mdr.downloaders.DocxDownloader']}})
+@override_settings(ARISTOTLE_SETTINGS={'DOWNLOAD_OPTIONS': {'DOWNLOADERS': ['aristotle_mdr.downloaders.DocxDownloader']}})
 class DocxDownloaderTestCase(AristotleTestUtils, TestCase):
 
     def setUp(self):
